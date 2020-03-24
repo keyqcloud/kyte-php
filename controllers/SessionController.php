f2c32c6b2a83dd46bd89eed6a88ad1637c321399
Kenneth P. Hough <kenneth@egtech.us>
2020-03-24 15:05:16 +0900

    Create SessionController.php

---
+++ controllers/SessionController.php
@@ -0,0 +1,65 @@
+<?php
+
+class SessionController extends ModelController
+{
+    private $session;
+
+    public function __construct($token) {
+        try {
+            $this->session = new \Kyte\SessionManager(Session);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+    
+    // new session  :   {model}, {data}
+    public function new($model, $data, $dateformat)
+    {
+        $response = [];
+
+        try {
+            foreach (['email', 'password'] as $param) {
+				if (!isset($data[$param]))
+					throw new Exception("Incomplete data passed");
+			}
+			$response = [ 'token' => $this->session->create(Account, $data['email'], $data['password']) ];
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+        return $response;
+    }
+
+    // update   :   {model}, {field}, {value}, {data}
+    public function update($model, $field, $value, $data, $dateformat)
+    {
+        throw new \Exception("Undefined request method");
+    }
+
+    // get  :   {model}, {field}, {value}
+    public function get($model, $field, $value, $dateformat)
+    {
+        try {
+            $response = [ 'token' => $this->session->validate($value) ];
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+        return $response;
+    }
+
+    // delete   :   {model}, {field}, {value}
+    public function delete($model, $field, $value, $dateformat)
+    {
+        try {
+            $this->session->validate($value);
+            $this->session->destroy();
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+        return [ 'token' => '' ];
+    }
+}
+
+?>
