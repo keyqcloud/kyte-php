9422dae43d450ef9d7d496329026e9500913f84d
Kenneth P. Hough <kenneth@egtech.us>
2020-03-25 18:02:40 +0900

    Update signature signing and variable name

--- index.php
+++ index.php
@@ -52,16 +52,16 @@ try {
 
     // If minimum params are not passed, then generate signature and return
     if(count($elements) < 5) {
-        if(count($elements) == 1) {
-            /* POST     /{key} */
+        if(count($elements) == 2) {
+            /* GET     /{key}/{time}/{identifier} */
             $obj = new \Kyte\ModelObject(APIKey);
             if ($obj->retrieve('public_key', $elements[0])) {
             } else throw new Exception("Invalid API access key");
     
-            $date = new DateTime($data['kyte-time'], new DateTimeZone('UTC'));
+            $date = new DateTime(urldecode($elements[1]), new DateTimeZone('UTC'));
     
             $hash1 = hash_hmac('SHA256', $date->format('U'), $obj->getParam('secret_key'), true);
-            $hash2 = hash_hmac('SHA256', $data['kyte-identifier'], $hash1, true);
+            $hash2 = hash_hmac('SHA256', urldecode($elements[2]), $hash1, true);
             $response['signature'] = hash_hmac('SHA256', $elements[0], $hash2);
         } else {
             $response['version'] = \Kyte\ApplicationVersion::get();
@@ -79,8 +79,8 @@ try {
         $api->validate($elements[2], $date->format('U'));
 
         // initialize controller for model or view ("abstract" controller)
-        $controllerClass = class_exists($element[4]{'Controller'}) ? $element[4]{'Controller'} : 'ModelController';
-        $controller = new $controllerClass($element[4], APP_DATE_FORMAT, urldecode($element[0]));
+        $controllerClass = class_exists($elements[4]{'Controller'}) ? $elements[4]{'Controller'} : 'ModelController';
+        $controller = new $controllerClass($elements[4], APP_DATE_FORMAT, urldecode($elements[0]));
         if (!$controller) throw new Exception("[ERROR] Unable to create controller for model: $controllerClass.");
 
         switch ($request) {
@@ -91,17 +91,17 @@ try {
 
             case 'PUT':
                 // update   :   {field}, {value}, {data}
-                $response = $controller->update($element[5], $element[6], $data);
+                $response = $controller->update($elements[5], $elements[6], $data);
                 break;
 
             case 'GET':
                 // get  :   {field}, {value}
-                $response = $controller->get($element[5], $element[5]);
+                $response = $controller->get($elements[5], $elements[5]);
                 break;
 
             case 'DELETE':
                 // delete   :   {field}, {value}
-                $response = $controller->delete($element[5], $element[5]);
+                $response = $controller->delete($elements[5], $elements[5]);
                 break;
             
             default:
