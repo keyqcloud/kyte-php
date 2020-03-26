e1fceebf335900bb574ba24e4d979ea0b04de55c
Kenneth P. Hough <kenneth@egtech.us>
2020-03-26 15:17:56 +0900

    Fix error output

--- initializer.php
+++ initializer.php
@@ -10,17 +10,17 @@
 	// include models being used by app
     foreach (glob("models/*.php") as $filename) {
         require_once($filename);
-        error_log("Loading model $model_name");
         $model_name = substr($filename, 0, strrpos($filename, "."));
         $model_name = str_replace('models/','',$model_name);
+        error_log("Loading model $model_name");
         define($model_name, $$model_name);
     }
     
     // include any controllers
     require 'controllers/ModelController.php';
 	foreach (glob("controllers/*.php") as $filename) {
+        error_log("Loading controller $filename");
         require_once($filename) ;
-        error_log("Loading controller $model_name");
     }
 
     require_once __DIR__.'/config.php';
