b1f3824a7d268c3142ac4df2feda205b7b1fbd24
Kenneth P. Hough <kenneth@egtech.us>
2020-03-26 18:40:34 +0900

    Add code to load user-defined models and controllers that can be cloned from a VCS

--- initializer.php
+++ initializer.php
@@ -23,20 +23,26 @@
         require_once($filename) ;
     }
 
-    // load user defined models and controllers (allow override of builtin)
-    foreach (glob("app/models/*.php") as $filename) {
-        require_once($filename);
-        $model_name = substr($filename, 0, strrpos($filename, "."));
-        $model_name = str_replace('app/models/','',$model_name);
-        error_log("Loading model $model_name");
-        define($model_name, $$model_name);
-    }
-    
-    // include any controllers
-	foreach (glob("app/controllers/*.php") as $filename) {
-        error_log("Loading controller $filename");
-        require_once($filename) ;
-    }
+    /* Load user-defined files */
+    if ( file_exists( "app/" ) && !is_dir( "app/" ) ) {
+        if ( file_exists( "app/models/" ) && !is_dir( "app/models/" ) ) {
+            // load user defined models and controllers (allow override of builtin)
+            foreach (glob("app/models/*.php") as $filename) {
+                require_once($filename);
+                $model_name = substr($filename, 0, strrpos($filename, "."));
+                $model_name = str_replace('app/models/','',$model_name);
+                error_log("Loading model $model_name");
+                define($model_name, $$model_name);
+            }
+        }
+        if ( file_exists( "app/controllers/" ) && !is_dir( "app/controllers/" ) ) {
+            // include any controllers
+            foreach (glob("app/controllers/*.php") as $filename) {
+                error_log("Loading controller $filename");
+                require_once($filename) ;
+            }
+        }      
+    } 
 
     require_once __DIR__.'/config.php';
     
