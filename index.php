92c7c5fee453c9d0ff69292cb9946e49376e06b8
Kenneth P. Hough <kenneth@egtech.us>
2020-04-01 17:08:09 +0900

    Fix variable variable name definition for class

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ try {
         $api->validate($elements[2], $date->format('U'));
 
         // initialize controller for model or view ("abstract" controller)
-        $controllerClass = class_exists(${elements[4].'Controller'}) ? ${elements[4].'Controller'} : 'ModelController';
+        $controllerClass = class_exists(${$elements[4].'Controller'}) ? ${$elements[4].'Controller'} : 'ModelController';
         $controller = new $controllerClass($elements[4], APP_DATE_FORMAT, $elements[0]);
         if (!$controller) throw new Exception("[ERROR] Unable to create controller for model: $controllerClass.");
 
