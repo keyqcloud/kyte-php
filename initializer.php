7a1460adf6b19c1ef97f082dfad6aa1bd6a8b22d
Kenneth P. Hough <kenneth@egtech.us>
2020-04-01 17:26:10 +0900

    Add verbose checks when loading models and controllers

--- initializer.php
+++ initializer.php
@@ -13,13 +13,17 @@
         $model_name = substr($filename, 0, strrpos($filename, "."));
         $model_name = str_replace('builtin/models/','',$model_name);
         error_log("Loading model $model_name");
+        error_log("Checking if model has been defined...".(!isset($$model_name) ? 'defined!' : 'UNDEFINED!'));
         define($model_name, $$model_name);
     }
     
     // include any controllers
     require 'builtin/controllers/ModelController.php';
 	foreach (glob("builtin/controllers/*.php") as $filename) {
+        $controller_name = substr($filename, 0, strrpos($filename, "."));
+        $controller_name = str_replace('builtin/controllers/','',$controller_name);
         error_log("Loading controller $filename");
+        error_log("Checking if controller has been defined...".(class_exists($controller_name) ? 'defined!' : 'UNDEFINED!'));
         require_once($filename) ;
     }
 
@@ -31,14 +35,18 @@
                 require_once($filename);
                 $model_name = substr($filename, 0, strrpos($filename, "."));
                 $model_name = str_replace('app/models/','',$model_name);
-                error_log("Loading model $model_name");
+                error_log("Loading user defined model $model_name");
+                error_log("Checking if user defined model has been defined...".(isset($$model_name) ? 'defined!' : 'UNDEFINED!'));
                 define($model_name, $$model_name);
             }
         }
         if ( file_exists( "app/controllers/" ) && is_dir( "app/controllers/" ) ) {
             // include any controllers
             foreach (glob("app/controllers/*.php") as $filename) {
-                error_log("Loading controller $filename");
+                $controller_name = substr($filename, 0, strrpos($filename, "."));
+                $controller_name = str_replace('app/controllers/','',$controller_name);
+                error_log("Loading user defined controller $filename");
+                error_log("Checking if user defined controller has been defined...".(class_exists($controller_name) ? 'defined!' : 'UNDEFINED!'));
                 require_once($filename) ;
             }
         }      
