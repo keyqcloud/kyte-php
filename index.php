2d4c776f180a3b24e082ac19e3c073a1f06cfbf8
Kenneth P. Hough <kenneth@egtech.us>
2020-04-08 14:15:23 +0900

    Update cors

--- index.php
+++ index.php
@@ -15,21 +15,21 @@ $request = $_SERVER['REQUEST_METHOD'];
 
 error_log("Access from $origin for request method $request");
 
+header("Access-Control-Allow-Origin: $origin");
+header('Access-Control-Allow-Credentials: true');
+header("Content-Type: application/json; charset=utf-8");
+
 // Access-Control headers are received during OPTIONS requests
 if ($request == 'OPTIONS') {
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, HEAD");         
+        header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, HEAD, OPTION");         
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 
     exit(0);
 }
-
-header("Access-Control-Allow-Origin: $origin");
-header('Access-Control-Allow-Credentials: true');
-header("Content-Type: application/json; charset=utf-8");
 
 require_once __DIR__.'/initializer.php';
 
