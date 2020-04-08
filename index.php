3005b79652bc0dac593c04252b5868eb7dddadd7
Kenneth P. Hough <kenneth@egtech.us>
2020-04-08 14:11:37 +0900

    Update cors

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ error_log("Access from $origin for request method $request");
 if ($request == 'OPTIONS') {
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");         
+        header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, HEAD");         
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
