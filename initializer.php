9699a6993d4b199683bc2335fb732f3ed2aa9113
Kenneth P. Hough <kenneth@egtech.us>
2020-04-08 11:27:45 +0900

    Update initializer to allow userdef overrride

--- initializer.php
+++ initializer.php
@@ -4,30 +4,13 @@
 	require 'vendor/autoload.php';
 	// include any utility scripts
 	foreach (glob("util/*.php") as $filename) {
-		require_once($filename) ;
-    }
-    
-	// include models being used by app
-    foreach (glob("builtin/models/*.php") as $filename) {
-        require_once($filename);
-        $model_name = substr($filename, 0, strrpos($filename, "."));
-        $model_name = str_replace('builtin/models/','',$model_name);
-        error_log("Loading model $model_name");
-        error_log("Checking if model has been defined...".(isset($$model_name) ? 'defined!' : 'UNDEFINED!'));
-        define($model_name, $$model_name);
-    }
-    
-    // include any controllers
-    require 'builtin/controllers/ModelController.php';
-	foreach (glob("builtin/controllers/*.php") as $filename) {
-        $controller_name = substr($filename, 0, strrpos($filename, "."));
-        $controller_name = str_replace('builtin/controllers/','',$controller_name);
-        error_log("Loading controller $filename");
-        require_once($filename);
-        error_log("Checking if controller has been defined...".(class_exists($controller_name) ? 'defined!' : 'UNDEFINED!'));
+        require_once($filename) ;
     }
 
-    /* Load user-defined files */
+    // load base controller
+    require 'builtin/controllers/ModelController.php';
+    
+    /* Load user-defined files first in case there are overrides */
     if ( file_exists( "app/" ) && is_dir( "app/" ) ) {
         if ( file_exists( "app/models/" ) && is_dir( "app/models/" ) ) {
             // load user defined models and controllers (allow override of builtin)
@@ -51,6 +34,25 @@
             }
         }      
     } 
+
+	// include models being used by app
+    foreach (glob("builtin/models/*.php") as $filename) {
+        require_once($filename);
+        $model_name = substr($filename, 0, strrpos($filename, "."));
+        $model_name = str_replace('builtin/models/','',$model_name);
+        error_log("Loading model $model_name");
+        error_log("Checking if model has been defined...".(isset($$model_name) ? 'defined!' : 'UNDEFINED!'));
+        define($model_name, $$model_name);
+    }
+    
+    // include any controllers
+	foreach (glob("builtin/controllers/*.php") as $filename) {
+        $controller_name = substr($filename, 0, strrpos($filename, "."));
+        $controller_name = str_replace('builtin/controllers/','',$controller_name);
+        error_log("Loading controller $filename");
+        require_once($filename);
+        error_log("Checking if controller has been defined...".(class_exists($controller_name) ? 'defined!' : 'UNDEFINED!'));
+    }
 
     require_once __DIR__.'/config.php';
     
