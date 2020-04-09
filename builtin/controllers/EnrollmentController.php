b2c2c40c69fac853519ea428254abca671a442e2
Kenneth P. Hough <kenneth@egtech.us>
2020-04-09 11:29:28 +0900

    Update hash gen

--- builtin/controllers/EnrollmentController.php
+++ builtin/controllers/EnrollmentController.php
@@ -12,7 +12,9 @@ class EnrollmentController extends ModelController
 
         try {
             $obj = new \Kyte\ModelObject(Account);
-            $data['password'] = 't0k3n'.password_hash($data['password'], PASSWORD_DEFAULT);
+            $time = time();
+			$exp_time = $time+(60*60);
+            $data['password'] = 't0k3n'.hash_hmac('sha256', $data['email'].'-'.$time, $exp_time);
             if ($obj->create($data)) {
                 $response = $obj->getAllParams($this->dateformat);
             }
