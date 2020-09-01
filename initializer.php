fba22edafc4ad94bdea1664cac2d983366e91dd3
Kenneth P. Hough <kenneth@egtech.us>
2020-08-31 22:34:36 -0500

    Fix dir path

--- initializer.php
+++ initializer.php
@@ -15,23 +15,23 @@
 	define('APP_LANG', in_array($lang, $acceptLang) ? $lang : 'en');
 
     /* load classes from composer */
-	require __DIR__ . 'vendor/autoload.php';
+	require __DIR__ . '/vendor/autoload.php';
 	// include any utility scripts
-	foreach (glob(__DIR__ . "util/*.php") as $filename) {
+	foreach (glob(__DIR__ . "/util/*.php") as $filename) {
         require_once($filename) ;
     }
 
     // load base controller
-    require __DIR__ . 'builtin/controllers/ModelController.php';
+    require __DIR__ . '/builtin/controllers/ModelController.php';
     
     /* Load user-defined files first in case there are overrides */
-    if ( file_exists( __DIR__ . "app/" ) && is_dir( __DIR__ . "app/" ) ) {
-        if ( file_exists( __DIR__ . "app/models/" ) && is_dir( __DIR__ . "app/models/" ) ) {
+    if ( file_exists( __DIR__ . "/app/" ) && is_dir( __DIR__ . "/app/" ) ) {
+        if ( file_exists( __DIR__ . "/app/models/" ) && is_dir( __DIR__ . "/app/models/" ) ) {
             // load user defined models and controllers (allow override of builtin)
-            foreach (glob(__DIR__ . "app/models/*.php") as $filename) {
+            foreach (glob(__DIR__ . "/app/models/*.php") as $filename) {
                 require_once($filename);
                 $model_name = substr($filename, 0, strrpos($filename, "."));
-                $model_name = str_replace(__DIR__ . 'app/models/','',$model_name);
+                $model_name = str_replace(__DIR__ . '/app/models/','',$model_name);
                 if (VERBOSE_LOG) {
                     error_log("Loading user defined model $model_name");
                     error_log("Checking if user defined model has been defined...".(isset($$model_name) ? 'defined!' : 'UNDEFINED!'));
@@ -39,11 +39,11 @@
                 define($model_name, $$model_name);
             }
         }
-        if ( file_exists( __DIR__ . "app/controllers/" ) && is_dir( __DIR__ . "app/controllers/" ) ) {
+        if ( file_exists( __DIR__ . "/app/controllers/" ) && is_dir( __DIR__ . "/app/controllers/" ) ) {
             // include any controllers
-            foreach (glob(__DIR__ . "app/controllers/*.php") as $filename) {
+            foreach (glob(__DIR__ . "/app/controllers/*.php") as $filename) {
                 $controller_name = substr($filename, 0, strrpos($filename, "."));
-                $controller_name = str_replace(__DIR__ . 'app/controllers/','',$controller_name);
+                $controller_name = str_replace(__DIR__ . '/app/controllers/','',$controller_name);
                 require_once($filename);
                 if (VERBOSE_LOG) {
                     error_log("Checking if user defined controller has been defined...".(class_exists($controller_name) ? 'defined!' : 'UNDEFINED!'));
@@ -53,9 +53,9 @@
     } 
 
 	// include models being used by app
-    foreach (glob(__DIR__ . "builtin/models/*.php") as $filename) {
+    foreach (glob(__DIR__ . "/builtin/models/*.php") as $filename) {
         $model_name = substr($filename, 0, strrpos($filename, "."));
-        $model_name = str_replace(__DIR__ . 'builtin/models/','',$model_name);
+        $model_name = str_replace(__DIR__ . '/builtin/models/','',$model_name);
         if (isset($$model_name)) {
             if (VERBOSE_LOG) {
                 error_log("Skipping model $model_name as already defined...");
@@ -70,9 +70,9 @@
     }
     
     // include any controllers
-	foreach (glob(__DIR__ . "builtin/controllers/*.php") as $filename) {
+	foreach (glob(__DIR__ . "/builtin/controllers/*.php") as $filename) {
         $controller_name = substr($filename, 0, strrpos($filename, "."));
-        $controller_name = str_replace(__DIR__ . 'builtin/controllers/','',$controller_name);
+        $controller_name = str_replace(__DIR__ . '/builtin/controllers/','',$controller_name);
         if (class_exists($controller_name)) {
             if (VERBOSE_LOG) {
                 error_log("Skipping controller $filename as already defined...");
