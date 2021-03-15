e5afdc27ad432ad852c0326e5b0a6080d3c73b06
Kenneth P. Hough <kenneth@egtech.us>
2021-03-15 14:18:00 -0500

    Return api handoff while in session

--- index.php
+++ index.php
@@ -190,6 +190,25 @@ try {
                 throw new SessionException("Invalid user session.");
             }
             $response['sessionPermission'] = $user->getParam('role');
+
+            // check is user has different account
+            // get user account
+            if ($user->getParam('kyte_account') != $account->getParam('id')) {
+                if (!$account->retrieve('id', $user->getParam('kyte_account'))) {
+                    throw new Exception("Unable to find account associated with user");
+                }
+            }
+
+            // get api associated with account
+            $account_api = new \Kyte\ModelObject(APIKey);
+            if (!$account_api->retrieve('kyte_account', $account->getParam('id'))) {
+                throw new Exception("[ERROR] Unable to find API information for account");
+            }
+
+            // return account information in response - this is required for API handoff between master account and subaccounts
+            $response['kyte_pub'] = $account_api->getParam('public_key');
+            $response['kyte_num'] = $account->getParam('number');
+            $response['kyte_iden'] = $account_api->getParam('identifier');
         }
 
         // update log with tx token
