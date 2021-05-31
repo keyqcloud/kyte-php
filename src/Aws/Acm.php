b0e45beb3c97a50ad401ea116ed77e6aa92c7329
Kenneth P. Hough <kenneth@egtech.us>
2021-05-31 15:52:06 -0500

    Add Acm wrapper

---
+++ src/Aws/Acm.php
@@ -0,0 +1,70 @@
+<?php
+namespace Kyte\Aws;
+
+use Aws\Exception\AwsException;
+use Aws\Acm\AcmClient;
+
+private $credentials;
+private $client;
+
+class Acm
+{
+    public function __construct($credentials) {
+        $this->credentials = $credentials;
+        $this->client = new AcmClient([
+            'credentials'	=> $this->credentials->getCredentials(),
+            'version'	=> '2014-11-01',
+            'region'	=> $this->credentials->getRegion()
+        ]);
+    }
+
+    public function request($domainName, $san = []) {
+        // create idempotency token
+        $idemToken = $domainName.time();
+
+        // request certificate
+        $result = $this->client->requestCertificate([
+            'DomainName' => $domainName, // REQUIRED
+            'SubjectAlternativeNames' => $san,
+            'IdempotencyToken' => $idemToken,
+            'ValidationMethod' => 'DNS',
+        ]);
+
+        if (!isset($result['CertificateArn'])) {
+            throw new \Exception('Unable to create new certificate');
+        }
+
+        return [ 'arn' => $result['CertificateArn'], 'IdempotencyToken' => $idemToken ];
+    }
+
+    public function describe($arn) {
+        $result = $this->client->describeCertificate([
+            'CertificateArn' => $arn, // REQUIRED
+        ]);
+
+        // CNAME records required
+        // 'DomainValidationOptions' => [
+        //     [
+        //         'DomainName' => '<string>',
+        //         'ResourceRecord' => [
+        //             'Name' => '<string>',
+        //             'Type' => 'CNAME',
+        //             'Value' => '<string>',
+        //     ],
+
+        return $result;
+    }
+
+    public function delete($arn) {
+        $result = $client->deleteCertificate([
+            'CertificateArn' => $arn, // REQUIRED
+        ]);
+    }
+
+    public function list($arn, $params = []) {
+        $result = $this->client->listCertificates($params);
+        
+        return $result;
+    }
+}
+?>
