3a28d7051a768c7416b92b5a3b10073162980b3e
Kenneth P. Hough <kenneth@egtech.us>
2021-05-31 13:55:37 -0500

    Add wrapper for AWS KMS

---
+++ src/Aws/Kms.php
@@ -0,0 +1,48 @@
+<?php
+namespace Kyte\Aws;
+
+private $credentials;
+private $KmsClient;
+private $kmsKeyId;
+
+class Kms
+{
+    public function __construct($credentials, $kmsKeyId) {
+        $this->credentials = $credentials;
+        $this->kmsKeyId = $kmsKeyId;
+        $this->KmsClient = new Aws\Kms\KmsClient([
+            'credential'	=> $this->credentials->getCredentials(),
+            'version'	=> '2014-11-01',
+            'region'	=> $this->credentials->getRegion()
+        ]);
+    }
+
+    public function encrypt($data) {
+        if (is_array($data)) {
+            $data = json_encode($data);
+        }
+        $aws_res = $this->KmsClient->encrypt([
+            'KeyId' => $this->kmsKeyId,
+            'Plaintext' => $data,
+        ]);
+        
+        if (!isset($aws_res["CiphertextBlob"])) {
+            throw new \Exception("Did not return cipher");
+        }
+
+        return $aws_res["CiphertextBlob"];
+    }
+
+    public function decrypt($cipher) {
+        $aws_res = $this->KmsClient->decrypt([
+            'CiphertextBlob' => $cipher,
+        ]);
+
+        if (!isset($aws_res["Plaintext"])) {
+            throw new \Exception("Did not return plaintext");
+        }
+
+        return $aws_res['Plaintext'];
+    }
+}
+?>
