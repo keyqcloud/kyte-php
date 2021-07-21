de64322e0f8a4ee855a70682a09391c985ac97d7
Kenneth P. Hough <kenneth@egtech.us>
2021-07-21 17:20:09 -0500

    Add dummy unit test

---
+++ tests/FunctionTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Kyte\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class FunctionsTest extends TestCase
+{
+    public function testFunction() {
+        $this->assertTrue(false);
+    }
+}
+
+?>  
