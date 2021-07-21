bc901dca5b7181cfa4e3f59cc0d116b81ecb4dc4
Kenneth P. Hough <kenneth@egtech.us>
2021-07-21 18:21:54 -0500

    Change to reflect provisioned container test

---
+++ tests/DatabaseTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Kyte\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    public function testDatabaseCreation() {
+        $this->assertTrue(\Kyte\Core\DBI::create("TestClientApp"));
+    }
+}
+
+?>  
