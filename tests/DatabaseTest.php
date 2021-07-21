d31632c49b085453a140553b5b69ca286232147d
Kenneth P. Hough <kenneth@egtech.us>
2021-07-21 18:28:02 -0500

    Update to assign model and privs

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 class DatabaseTest extends TestCase
 {
     public function testDatabaseCreation() {
-        $this->assertTrue(\Kyte\Core\DBI::create("TestClientApp"));
+        $this->assertTrue(\Kyte\Core\DBI::create("TestModel", "TestUser", $password));
     }
 }
 
--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 class ModelTest extends TestCase
 {
     public function testCreateTable() {
-        $this->assertTrue(\Kyte\Core\DBI::create("TestModel", "TestUser", $password));
+        $this->assertTrue(\Kyte\Core\DBI::create("TestModel2", "TestUser2", $password));
     }
 }
 
