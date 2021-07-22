1ea202365b209ed7b65fa3fa8751eb318aa944ef
Kenneth P. Hough <kenneth@egtech.us>
2021-07-21 21:01:46 -0500

    Create a test model

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -7,7 +7,79 @@ class ModelTest extends TestCase
 {
     public function testCreateTable() {
         // create test user table
-        $this->assertTrue(\Kyte\Core\DBI::createTable(User));
+        // $this->assertTrue(\Kyte\Core\DBI::createTable(User));
+
+        // sample model
+        $TestTable = [
+            'name' => 'TestTable',
+            'struct' => [
+                'name'		=> [
+                    'type'		=> 's',
+                    'required'	=> true,
+                    'size'		=> 255,
+                    'date'		=> false,
+                ],
+        
+                // framework attributes
+        
+                'kyte_account'	=> [
+                    'type'		=> 'i',
+                    'required'	=> true,
+                    'size'		=> 11,
+                    'unsigned'	=> true,
+                    'date'		=> false,
+                ],
+        
+                // audit attributes
+        
+                'created_by'		=> [
+                    'type'		=> 'i',
+                    'required'	=> false,
+                    'date'		=> true,
+                ],
+        
+                'date_created'		=> [
+                    'type'		=> 'i',
+                    'required'	=> false,
+                    'date'		=> true,
+                ],
+        
+                'modified_by'		=> [
+                    'type'		=> 'i',
+                    'required'	=> false,
+                    'date'		=> true,
+                ],
+        
+                'deleted_by'		=> [
+                    'type'		=> 'i',
+                    'required'	=> false,
+                    'date'		=> true,
+                ],
+        
+                'created_by'		=> [
+                    'type'		=> 'i',
+                    'required'	=> false,
+                    'date'		=> true,
+                ],
+        
+                'date_deleted'		=> [
+                    'type'		=> 'i',
+                    'required'	=> false,
+                    'date'		=> true,
+                ],
+        
+                'deleted'	=> [
+                    'type'		=> 'i',
+                    'required'	=> false,
+                    'size'		=> 1,
+                    'unsigned'	=> true,
+                    'default'	=> 0,
+                    'date'		=> false,
+                ],
+            ],
+        ];
+        
+        $this->assertTrue(\Kyte\Core\DBI::createTable($TestTable));
     }
 }
 
