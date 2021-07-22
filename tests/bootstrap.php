22015d09f710e312266e2cf809a7b7a4330d2bf1
Kenneth P. Hough <kenneth@egtech.us>
2021-07-21 19:03:18 -0500

    Add db initialization

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,10 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+\Kyte\Core\DBI::setDbUser(KYTE_DB_USERNAME);
+\Kyte\Core\DBI::setDbPassword(KYTE_DB_PASSWORD);
+\Kyte\Core\DBI::setDbHost(KYTE_DB_HOST);
+\Kyte\Core\DBI::setDbName(KYTE_DB_DATABASE);
+\Kyte\Core\DBI::setCharset(KYTE_DB_CHARSET);
+
 ?>
