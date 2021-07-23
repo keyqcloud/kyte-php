af35a4c9138415151d5b628307209d2a2d91f84d
Kenneth P. Hough <kenneth@egtech.us>
2021-07-23 16:34:22 -0500

    Add SES test

---
+++ tests/AwsSesTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Kyte\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class AwsSesTest extends TestCase
+{
+    public function testEncryptAndDecrypt() {
+        $credential = new \Kyte\Aws\Credentials('us-east-1');
+        $this->assertIsObject($credential);
+
+        // create s3 client for private bucket
+        $ses = new \Kyte\Aws\Ses($credential, 'kenneth@keyqcloud.com');
+        $this->assertIsObject($ses);
+
+        $result = $ses->send(['kenneth@keyqcloud.com'], 'Test run from Travis CI', 'Testing SES send email');
+
+        $this->assertIsString($result);
+    }
+}
+
+?>  
