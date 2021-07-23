7f9d791f9591fb460234951edaa0eb57ca7cc5c7
Kenneth P. Hough <kenneth@egtech.us>
2021-07-23 15:52:52 -0500

    Add API unit test

---
+++ tests/ApiTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace Kyte\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class APiTest extends TestCase
+{
+    public function setUp() {
+        // setup account and apikey
+        require_once(__DIR__ . '../Mvc/Model/APIKey.php');
+        require_once(__DIR__ . '../Mvc/Model/Account.php');
+
+        $mockAPI = $APIKey;
+        $mockAccount = $Account;
+        // add primary keys
+        \Kyte\Core\Api::addPrimaryKey($mockAPI);
+        \Kyte\Core\Api::addPrimaryKey($mockAccount);
+
+        \Kyte\Core\DBI::createTable($mockAPI);
+        \Kyte\Core\DBI::createTable($mockAccount);
+
+        // create dummy key
+        $model = new \Kyte\Core\ModelObject($mockAPI);
+
+        $model->create([
+            'identifier' => 'FOO',
+            'public_key' => 'BAR',
+            'secret_key' => 'BAZ',
+            'epoch' => 0,
+            'kyte_account' => 1,
+        ]);
+
+        $model = new \Kyte\Core\ModelObject($mockAccount);
+
+        $model->create([
+            'name' => 'FOO',
+            'number' => 'BAR',
+        ]);
+    }
+    
+    public function testInitApiFail() {
+        $api = new \Kyte\Core\Api();
+        $this->assertFalse($api->init('FOO'));
+    }
+
+    public function testInitApiSuccess() {
+        $api = new \Kyte\Core\Api();
+        $this->assertTrue($api->init('BAR'));
+    }
+}
+
+?>  
