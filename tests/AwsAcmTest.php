90e10b47ce64b966648cc2161fe692170dedd335
Kenneth P. Hough <kenneth@egtech.us>
2021-07-23 18:29:26 -0500

    Pass arn instead of acm client object

--- tests/AwsAcmTest.php
+++ tests/AwsAcmTest.php
@@ -14,42 +14,62 @@ class AwsAcmTest extends TestCase
         $this->assertIsObject($acm);
 
         $result = $acm->request('www.perryhough.com', ['www.perryhough.com']);
-        $this->assertIsString($result['CertificateArn']);
+        $arn = $result['CertificateArn'];
+        $this->assertIsString($arn);
 
-        return $acm;
+        return $arn;
     }
 
     /*
      * @depends testRequestCertificate
      * */
-    public function testDescribeCertificate($acm) {
+    public function testDescribeCertificate($arn) {
+        $credential = new \Kyte\Aws\Credentials('us-east-1');
+        $this->assertIsObject($credential);
+
+        // create s3 client for private bucket
+        $acm = new \Kyte\Aws\Acm($credential, $arn);
+        $this->assertIsObject($acm);
+
         $certificate = $acm->describe();
 
         $this->assertEquals('www.perryhough.com', $certificate['Certificate']['DomainName']);
 
-        return $acm;
+        return $arn;
     }
 
     /*
      * @depends testDescribeCertificate
      * */
-    public function testListCertificate($acm) {
+    public function testListCertificate($arn) {
+        $credential = new \Kyte\Aws\Credentials('us-east-1');
+        $this->assertIsObject($credential);
+
+        // create s3 client for private bucket
+        $acm = new \Kyte\Aws\Acm($credential, $arn);
+        $this->assertIsObject($acm);
+
         $certificates = $acm->list();
 
         $this->assertCount(1, $certificates['CertificateSummaryList']);
 
         $this->assertEquals('www.perryhough.com', $certificates['CertificateSummaryList'][0]['DomainName']);
 
-        return $acm;
+        return $arn;
     }
 
     /*
      * @depends testListCertificate
      * */
-    public function testDeleteCertificate($acm) {
+    public function testDeleteCertificate($arn) {
+        $credential = new \Kyte\Aws\Credentials('us-east-1');
+        $this->assertIsObject($credential);
+
+        // create s3 client for private bucket
+        $acm = new \Kyte\Aws\Acm($credential, $arn);
+        $this->assertIsObject($acm);
+
         $this->assertTrue($acm->delete());
-        
-        return $acm;
     }
 }
 
