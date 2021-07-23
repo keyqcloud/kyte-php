6a687d2c340a3ae16920f5a65796a7e2c4666b0b
Kenneth P. Hough <kenneth@egtech.us>
2021-07-23 14:01:36 -0500

    Add test for AWS credentials

---
+++ tests/AwsCredentialTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Kyte\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class AwsCredentialTest extends TestCase
+{
+    public function testCredential() {
+        $credential = new \Kyte\Aws\Credentials('us-east-1');
+        $this->assertIsObject($credential);
+
+        $this->assertEquals('us-east-1', $credential->getRegion);
+
+        $this->assertEquals(hash_hmac('sha1', AWS_ACCESS_KEY_ID, 'us-east-1'), hash_hmac('sha1', $credential->getAccessKey, 'us-east-1'));
+
+        $this->assertEquals(hash_hmac('sha1', AWS_SECRET_KEY, 'us-east-1'), hash_hmac('sha1', $credential->getSecretKey, 'us-east-1'));
+    }
+}
+
+?>  
