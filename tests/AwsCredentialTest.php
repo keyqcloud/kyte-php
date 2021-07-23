b0397a55b0347ebd02103a98d050aa47419f779e
Kenneth P. Hough <kenneth@egtech.us>
2021-07-23 14:31:21 -0500

    Addd missing paren to makemethod call

--- tests/AwsCredentialTest.php
+++ tests/AwsCredentialTest.php
@@ -9,11 +9,11 @@ class AwsCredentialTest extends TestCase
         $credential = new \Kyte\Aws\Credentials('us-east-1');
         $this->assertIsObject($credential);
 
-        $this->assertEquals('us-east-1', $credential->getRegion);
+        $this->assertEquals('us-east-1', $credential->getRegion());
 
-        $this->assertEquals(hash_hmac('sha1', AWS_ACCESS_KEY_ID, 'us-east-1'), hash_hmac('sha1', $credential->getAccessKey, 'us-east-1'));
+        $this->assertEquals(hash_hmac('sha1', AWS_ACCESS_KEY_ID, 'us-east-1'), hash_hmac('sha1', $credential->getAccessKey(), 'us-east-1'));
 
-        $this->assertEquals(hash_hmac('sha1', AWS_SECRET_KEY, 'us-east-1'), hash_hmac('sha1', $credential->getSecretKey, 'us-east-1'));
+        $this->assertEquals(hash_hmac('sha1', AWS_SECRET_KEY, 'us-east-1'), hash_hmac('sha1', $credential->getSecretKey(), 'us-east-1'));
     }
 }
 
