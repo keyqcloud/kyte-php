aa719a52febd7b359da294132f2404c64f841618
Kenneth Hough <kenneth@egtech.us>
2021-07-26 18:07:44 -0500

    Update bucket names to better differentiate

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,8 +13,8 @@ require __DIR__ . '/../vendor/autoload.php';
 define('AWS_ACCESS_KEY_ID', getenv('AWS_ACCESS_KEY_ID'));
 define('AWS_SECRET_KEY', getenv('AWS_SECRET_KEY'));
 // define('AWS_KMS_KEYID', getenv('AWS_KMS_KEYID'));
-define('AWS_PRIVATE_BUCKET_NAME', 'kyte-travisci-test-bucket-'.time());
-define('AWS_PUBLIC_BUCKET_NAME', 'kyte-travisci-test-bucket-'.time());
-define('AWS_TEST_SITE_NAME', 'kyte-travisci-test-bucket-'.time());
+define('AWS_PRIVATE_BUCKET_NAME', 'kyte-travisci-test-private-bucket-'.time());
+define('AWS_PUBLIC_BUCKET_NAME', 'kyte-travisci-test-public-bucket-'.time());
+define('AWS_TEST_SITE_NAME', 'kyte-travisci-test-static-site-bucket-'.time());
 
 ?>
