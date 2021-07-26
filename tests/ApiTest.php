a29fcccf46bf7a9295ab241f658ebc24ab9aa45d
Kenneth P. Hough <kenneth@egtech.us>
2021-07-26 12:16:44 -0500

    Move creation of test api and account to test case

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -4,14 +4,29 @@ namespace Kyte\Test;
 use PHPUnit\Framework\TestCase;
 
 class APiTest extends TestCase
-{   
-    public function testInitApiFail() {
-        $api = new \Kyte\Core\Api();
-        $this->assertFalse($api->init('FOO'));
-    }
+{
 
     public function testInitApiSuccess() {
+        // create new api
         $api = new \Kyte\Core\Api();
+
+        // create test api key
+        $model = new \Kyte\Core\ModelObject(APIKey);
+        $model->create([
+            'identifier' => 'FOO',
+            'public_key' => 'BAR',
+            'secret_key' => 'BAZ',
+            'epoch' => 0,
+            'kyte_account' => 1,
+        ]);
+
+        // create test account
+        $model = new \Kyte\Core\ModelObject(Account);
+        $model->create([
+            'name' => 'FOO',
+            'number' => 'BAR',
+        ]);
+
         $this->assertTrue($api->init('BAR'));
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,35 +15,4 @@ define('AWS_SECRET_KEY', getenv('AWS_SECRET_KEY'));
 define('AWS_KMS_KEYID', getenv('AWS_KMS_KEYID'));
 define('AWS_PRIVATE_BUCKET_NAME', 'kyte-travisci-test-bucket-'.time());
 
-// create mock apikey and account
-require_once(__DIR__ . '/../src/Mvc/Model/APIKey.php');
-require_once(__DIR__ . '/../src/Mvc/Model/Account.php');
-
-$mockAPI = $APIKey;
-$mockAccount = $Account;
-// add primary keys
-\Kyte\Core\Api::addPrimaryKey($mockAPI);
-\Kyte\Core\Api::addPrimaryKey($mockAccount);
-
-\Kyte\Core\DBI::createTable($mockAPI);
-\Kyte\Core\DBI::createTable($mockAccount);
-
-// create dummy key
-$model = new \Kyte\Core\ModelObject($mockAPI);
-
-$model->create([
-    'identifier' => 'FOO',
-    'public_key' => 'BAR',
-    'secret_key' => 'BAZ',
-    'epoch' => 0,
-    'kyte_account' => 1,
-]);
-
-$model = new \Kyte\Core\ModelObject($mockAccount);
-
-$model->create([
-    'name' => 'FOO',
-    'number' => 'BAR',
-]);
-
 ?>
