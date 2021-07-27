03f9b58a6d76c0180cb0ea740f003d0076e019e6
Kenneth P. Hough <kenneth@egtech.us>
2021-07-27 12:35:03 -0500

    Comment out KMS testing to save costs in the meantime

--- tests/AwsKmsTest.php
+++ tests/AwsKmsTest.php
@@ -6,24 +6,24 @@ use PHPUnit\Framework\TestCase;
 class AwsKmsTest extends TestCase
 {
     public function testEncryptAndDecrypt() {
-        $credential = new \Kyte\Aws\Credentials('us-east-1');
-        $this->assertIsObject($credential);
+        // $credential = new \Kyte\Aws\Credentials('us-east-1');
+        // $this->assertIsObject($credential);
 
         // create s3 client for private bucket
         // $kms = new \Kyte\Aws\Kms($credential, AWS_KMS_KEYID);
-        $kms = new \Kyte\Aws\Kms($credential);
-        $this->assertIsObject($kms);
+        // $kms = new \Kyte\Aws\Kms($credential);
+        // $this->assertIsObject($kms);
 
         // attempt to create a new symmetric CMK
-        $this->assertTrue($kms->createKey());
+        // $this->assertTrue($kms->createKey());
 
         // encrypt and decrypt
-        $cipher = $kms->encrypt('Test');
-        $text = $kms->decrypt($cipher);
-        $this->assertEquals('Test', $text);
+        // $cipher = $kms->encrypt('Test');
+        // $text = $kms->decrypt($cipher);
+        // $this->assertEquals('Test', $text);
 
         // schedule key deletion - default to 7 days
-        $this->assertTrue($kms->scheduleDelete());
+        // $this->assertTrue($kms->scheduleDelete());
     }
 }
 
