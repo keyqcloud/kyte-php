f979fdc90a954a1ed594a73608ae6644aa4ce145
Kenneth P. Hough <kenneth@egtech.us>
2022-09-15 18:18:38 -0500

    Add update and delete support

--- src/Mvc/Controller/ControllerController.php
+++ src/Mvc/Controller/ControllerController.php
@@ -44,6 +44,52 @@ class ControllerController extends ModelController
                 break;
 
             case 'update':
+                if ($o->name != $r['name']) {
+                    // check if new name is unique
+                    if (class_exists('\\Kyte\Mvc\\Controller\\'.$r['name'].'Controller')) {
+                        throw new \Exception("Controller name already in use by Kyte core API.");
+                    }
+                    if (class_exists($r['name'].'Controller')){
+                        throw new \Exception("Custom controller name already in use.");
+                    }
+                }
+
+                $functions = [];
+
+                // check if model is specified
+                if (!empty($r['dataModel'])) {
+                    $model = new \Kyte\Core\ModelObject(DataModel);
+                    if (!$model->retrieve("id", $r['dataModel'])) {
+                        throw new \Exception("Unable to find specified data model.");
+                    }
+
+                    $functions[] = "\tpublic function shipyard_init() {\r\n$\t\$this->model = {$model->name};\r\n}\r\n";
+                }
+
+                // regenerate code base with new name and/or model
+                $fs = new \Kyte\Core\Model(ControllerFunction);
+                $fs->retrieve("controller", $o->id);
+                foreach($fs->objects as $fc) {
+                    $f = new \Kyte\Core\ModelObject(constant("Function"));
+                    if (!$f->retrieve("id", $fc->function)) {
+                        throw new \Exception("Unable to find associated function.");
+                    }
+                    $functions[] = $f->code;
+                }
+
+                // update code base
+                $code = self::generateCodeBase($r['name'].'Controller', $functions);
+
+                if ($o->name != $r['name']) {
+                    // remove old definition
+                    if (!unlink("/var/www/html/app/models/{$o->name}.php")) {
+                        error_log("Failed to clean up old model /var/www/html/app/models/{$o->name}.php");
+                    }
+                }
+
+                if (file_put_contents("/var/www/html/app/controllers/{$r['name']}Controller.php", $code) === false) {
+                    throw new \Exception("Failed to create controller code! Squawk 7700!");
+                }
                 break;                
 
             default:
@@ -54,6 +100,19 @@ class ControllerController extends ModelController
     public function hook_response_data($method, $o, &$r = null, &$d = null) {
         switch ($method) {
             case 'delete':
+                // delete corresponding function association
+                $fs = new \Kyte\Core\Model(ControllerFunction);
+                $fs->retrieve("controller", $o->id);
+                foreach($fs->objects as $fc) {
+                    $fc->delete();
+                }
+
+                // delete associated settings
+                $settings = new \Kyte\Core\Model(ControllerSetting);
+                $settings->retrieve("controller", $o->id);
+                foreach($settings->objects as $setting) {
+                    $setting->delete();
+                }
                 break;
             
             default:
