0b90538437abd10daaec109f2f08dd87e898c776
Kenneth P. Hough <kenneth@egtech.us>
2022-09-15 16:54:52 -0500

    Rename model

--- src/Mvc/Model/ControllerSettingOption.php
+++ src/Mvc/Model/ControllerSettingOption.php
@@ -1,7 +1,7 @@
 <?php
 
-$ControllerSettingList = [
-	'name' => 'ControllerSettingList',
+$ControllerSettingOption = [
+	'name' => 'ControllerSettingOption',
 	'struct' => [
 		'name'		=> [
 			'type'		=> 's',
