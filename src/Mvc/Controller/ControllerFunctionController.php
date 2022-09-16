beb754d4c72e4fc64f44084e0de4882eb16f0bb7
Kenneth P. Hough <kenneth@egtech.us>
2022-09-15 20:11:20 -0500

    Add method to regenerate controller code

--- src/Mvc/Controller/ControllerFunctionController.php
+++ src/Mvc/Controller/ControllerFunctionController.php
@@ -18,9 +18,27 @@ class ControllerFunctionController extends ModelController
                 // get the function and populate the type
                 $func = new \Kyte\Core\ModelObject(constant("Function"));
                 if (!$func->retrieve("id", $r['function'])) {
-                    throw new \Exception("Unable to find specified data model.");
+                    throw new \Exception("Unable to find specified function.");
                 }
                 $r['type'] = $func->type;
+
+                $ctrl = new \Kyte\Core\ModelObject(constant("Controller"));
+                if (!$ctrl->retrieve("id", $r['controller'])) {
+                    throw new \Exception("Unable to find specified controller.");
+                }
+
+                $functions = [];
+
+                // check if model is specified
+                if (!empty($ctrl->dataModel)) {
+                    $functions[] = ControllerController::generateShipyardInit($ctrl->dataModel);
+                }
+
+                // regenerate code base with new name and/or model
+                ControllerController::prepareFunctionStatements($ctrl->id, $functions);
+
+                // update code base and save to file
+                ControllerController::generateCodeBase($ctrl->name.'Controller', $functions);
 
                 break;        
 
