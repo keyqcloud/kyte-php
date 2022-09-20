a2f7471cb1312ee8cebf2bef87838b7ec6f570ef
Kenneth P. Hough <kenneth@egtech.us>
2022-09-20 14:11:25 -0500

    Update log output

--- src/Core/Model.php
+++ src/Core/Model.php
@@ -80,7 +80,7 @@ class Model
 
 			$join = null;
 			$page_sql = "";
-			error_log("*********** {$this->search_fields}");
+			error_log("*********** {$this->search_values}");
 			if (isset($this->search_fields, $this->search_values)) {
 				error_log("*********** {$this->search_fields}");
 				$search_fields = explode(",", $this->search_fields);
