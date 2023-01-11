dc1634b6fd1fda5467e26ce2d3cb7a391463d76e
Kenneth Hough <kenneth@keyqcloud.com>
2023-01-11 03:21:30 -0600

    add page controller

--- src/Mvc/Controller/PageController.php
+++ src/Mvc/Controller/PageController.php
@@ -48,7 +48,7 @@ class PageController extends ModelController
                     $s3 = new \Kyte\Aws\S3($credential, $r['site']['s3BucketName']);
 
                     // compile html file
-                    $data = '';
+                    $data = $this->createHtml($o, $d['html'], $d['obfuscated']);
                     // write to file
                     $s3->write($o->s3key, $data);
 
@@ -78,4 +78,73 @@ class PageController extends ModelController
     }
 
     // public function hook_process_get_response(&$r) {}
+
+    private function createHtml($page, $html, $js) {
+        $html = '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"><title>'.$page->title.'</title>';
+        
+        // bootstrap
+        $html .= '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">';
+        $html .= '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>';
+        
+        // font aweseom
+        $html .= '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">';
+        // ionicons
+        $html .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">';
+        
+        // datatables
+        $html .= '<link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">';
+        $html .= '<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>';
+
+        // JQuery
+        $html .= '<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>';
+
+        // JQueryUI
+        $html .= '<link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">';
+        $html .= '<script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js" integrity="sha256-hlKLmzaRlE8SCJC1Kw8zoUbU8BxA+8kR3gseuKfMjxA=" crossorigin="anonymous"></script>';
+
+        // KyteJS
+        $html .= '<script src="https://cdn.stratis-troika.com/kytejs/2.0.0/kyte.js" crossorigin="anonymous"></script>';
+
+        // custom js
+        $html .= '<script>'.$js.'</script>';
+
+        // close head
+        $html .= '</head>';
+
+        // body
+        $html .= '<body>';
+
+        // loader
+        $html .= '<!-- Page loading modal.  Once session is validated, the loading modal will close. --><div id="pageLoaderModal" class="modal white" data-backdrop="static" data-keyboard="false" tabindex="-1"><div class="modal-dialog modal-sm h-100 d-flex"><div class="mx-auto align-self-center" style="width: 48px"><div class="spinner-wrapper text-center fa-6x"><span class="fas fa-sync fa-spin"></span></div></div></div></div><!--  -->';
+
+        // wrapper
+        $html .= '<div id="wrapper">';
+
+        // main navigation and header
+        if ($page->header) {
+            $html .= '<!-- START NAV --><nav id="mainnav" class="navbar navbar-dark bg-dark navbar-expand-lg"></nav><!-- END NAV -->';
+        }
+
+        // main wrapper
+        $html .= '<main>';
+
+        // side navigation
+        if ($page->navigation) {
+            $html .= '<!-- BEGIN SIDE NAVIGATION --><div id="sidenav" class="d-flex flex-column flex-shrink-0 p-3 text-white" style="width: 230px;"></div><!-- END SIDE NAVIGATION -->';
+        }
+
+        $html .= '<div class="container container-flex mb-5 px-5">'.$html.'</div>';
+
+        // close main wrapper
+        $html .= '</main>';
+        // close page wrapper
+        $html .= '</div>';
+
+        // close body
+        $html .= '</body>';
+        // close html
+        $html .= '</html>';
+
+        return $html;
+    }
 }
