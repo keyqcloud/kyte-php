bfdd0c167b6d0482381479534ed291656b54acb5
Kenneth Hough <kenneth@keyqcloud.com>
2023-01-12 15:09:47 -0600

    Add feature to request arn

--- src/Mvc/Controller/DomainController.php
+++ src/Mvc/Controller/DomainController.php
@@ -12,7 +12,46 @@ class DomainController extends ModelController
 
     // public function hook_preprocess($method, &$r, &$o = null) {}
 
-    // public function hook_response_data($method, $o, &$r = null, &$d = null) {}
+    public function hook_response_data($method, $o, &$r = null, &$d = null) {
+        // keep region as us-east-1 for compatibility with CF
+        $credential = new \Kyte\Aws\Credentials('us-east-1');
+
+        // create acm client
+        $acm = new \Kyte\Aws\Acm($credential);
+
+        switch ($method) {
+            case 'new':
+                $len = count($d['name']);
+
+                $obj = new \Kyte\Core\ModelObject(SubjectAlternativeName);
+
+                for ($i = 0; $i < $len; $i++) {
+                    if (!obj->create([
+                        "name" => $d['name'][$i],
+                        'domain' => $o->id,
+                        'kyte_account' => $this->account->id,
+                    ])) {
+                        $o->delete();
+                        throw new \Exception("Unable to create subject alternative name(s).");
+                    }
+                }
+
+                // make cert request
+                $r['certificateArn'] = $acm->request($r['domainName'], $d['name']);
+
+                // update domain certificate arn
+                $o->save([
+                    'certificateArn' => $r['certificateArn'],
+                ]);
+
+            case 'get':
+                $result = $acm->describe($r['certificateArn']);
+
+                $r['status'] = $results['Certificate']['Status'];
+
+                break;
+        }
+    }
 
     // public function hook_process_get_response(&$r) {}
 }
