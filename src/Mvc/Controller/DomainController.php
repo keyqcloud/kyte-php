4ab8394b704379d77c65552b7ab69e0d7f03d6f9
Kenneth P. Hough <kenneth@egtech.us>
2023-01-12 11:44:36 -0600

    Add placeholder for domain controller

---
+++ src/Mvc/Controller/DomainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kyte\Mvc\Controller;
+
+class DomainController extends ModelController
+{
+    // public function hook_init() {}
+
+    // public function hook_auth() {}
+
+    // public function hook_prequery($method, &$field, &$value, &$conditions, &$all, &$order) {}
+
+    // public function hook_preprocess($method, &$r, &$o = null) {}
+
+    // public function hook_response_data($method, $o, &$r = null, &$d = null) {}
+
+    // public function hook_process_get_response(&$r) {}
+}
