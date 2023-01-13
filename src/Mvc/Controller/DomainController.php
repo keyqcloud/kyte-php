ec4816bfe6533d6063bb9acee4505a8fa35fe777
Kenneth Hough <kenneth@keyqcloud.com>
2023-01-13 16:26:45 -0600

    Add ability to assign or remove aliases for CF

--- src/Mvc/Controller/DomainController.php
+++ src/Mvc/Controller/DomainController.php
@@ -56,5 +56,67 @@ class DomainController extends ModelController
         }
     }
 
+    public function update($field, $value, $data)
+    {
+        $response = [];
+        
+        try {
+            if (!in_array('update', $this->allowableActions)) {
+                return;
+            }
+    
+            if (!$this->checkPermissions('update')) {
+                throw new \Exception('Permission Denied');
+            }
+
+            if ($field === null || $value === null) throw new \Exception("Field and Values params not set");
+
+            $obj = new \Kyte\Core\ModelObject($this->model);
+            if ($obj->retrieve($field, $value, $conditions, null, $all)) {
+                $ret = $this->getObject($obj);
+
+                // keep region as us-east-1 for compatibility with CF
+                $credential = new \Kyte\Aws\Credentials('us-east-1');
+                $cf = new \Kyte\Aws\CloudFront($credentials, $ret['site']['cfDistributionId']);
+
+                if (isset($data['assigned'])) {
+                    // create acm client
+                    $acm = new \Kyte\Aws\Acm($credential);
+
+                    // check if certificate has been issued
+                    $acm_result = $acm->describe($r['certificateArn']);
+                    if ($acm_result['Certificate']['Status'] != 'ISSUED') {
+                        throw new \Exception("Unable to assign domain and SSL to site because the SSL certificate has not been issued. Please complete the domain verification");
+                    }
+
+                    // assign alias
+                    $aliases = [];
+                    foreach($acm_result['Certificate']['DomainValidationOptions'] as $san) {
+                        $aliases[] = $san['ValidationDomain'];
+                    }
+                    $cf->setAliases($aliases, $obj->certificateArn);
+
+                } else {
+                    // unasigned alias
+                    $cf->removeAliases();
+                }
+                //
+                $ret['assigned'] = $data['assigned'];
+                $obj->save([
+                    'assigned' => $data['assigned'],
+                ]);
+                $response[] = $ret;
+            } else {
+                if ($this->failOnNull) {
+                    throw new \Exception($this->exceptionMessages['update']['failOnNull']);
+                }
+            }
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        $this->response['data'] = $response;
+    }
+
     // public function hook_process_get_response(&$r) {}
 }
