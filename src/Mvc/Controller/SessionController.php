d98637895f94429c4715301648f25fec52664137
Kenneth P. Hough <kenneth@egtech.us>
2023-06-12 10:54:11 -0500

    Handle app level session creation

--- src/Mvc/Controller/SessionController.php
+++ src/Mvc/Controller/SessionController.php
@@ -18,37 +18,58 @@ class SessionController extends ModelController
         $session = [];
 
         try {
-            // check for required params
-            foreach ([USERNAME_FIELD, PASSWORD_FIELD] as $param) {
-				if (!isset($data[$param]))
-					throw new \Exception("Incomplete data passed");
-			}
+            if ($this->api->appId != null && $this->api->app->user_model != null && $this->api->app->username_colname != null && $this->api->app->password_colname != null) {
+                // check for required params
+                foreach ([$this->api->app->username_colname, $this->api->app->password_colname] as $param) {
+                    if (!isset($data[$param]))
+                        throw new \Exception("Incomplete data passed");
+                }
 
-            // create session for user and obtain user information
-            $session = $this->session->create($data[USERNAME_FIELD], $data[PASSWORD_FIELD]);
-            $user = new \Kyte\Core\ModelObject(KyteUser);
-            if (!$user->retrieve('id', $session['uid'])) {
-                throw new \Exception("Unable to find user information");    
-            }
-            // get user account
-            $account = new \Kyte\Core\ModelObject(KyteAccount);
-            if (!$account->retrieve('id', $user->kyte_account)) {
-                throw new \Exception("Unable to find account associated with user");
-            }
+                // create session for user and obtain user information
+                $session = $this->session->create($data[$this->api->app->username_colname], $data[$this->api->app->password_colname]);
 
-            // set user and account in class
-            $this->user = $user;
-            $this->account = $account;
+                $user = new \Kyte\Core\ModelObject(constant($this->api->app->user_model));
+                if (!$user->retrieve('id', $session['uid'])) {
+                    throw new \Exception("Unable to find user information");    
+                }
 
-            if (USE_SESSION_MAP) {
-                $response = $this->getObject($user);
-            } else {
+                $this->user = $user;
+                
                 $response[] = $this->getObject($user);
+            } else {
+                // check for required params
+                foreach ([USERNAME_FIELD, PASSWORD_FIELD] as $param) {
+                    if (!isset($data[$param]))
+                        throw new \Exception("Incomplete data passed");
+                }
+
+                // create session for user and obtain user information
+                $session = $this->session->create($data[USERNAME_FIELD], $data[PASSWORD_FIELD]);
+
+                $user = new \Kyte\Core\ModelObject(KyteUser);
+                if (!$user->retrieve('id', $session['uid'])) {
+                    throw new \Exception("Unable to find user information");    
+                }
+                // get user account
+                $account = new \Kyte\Core\ModelObject(KyteAccount);
+                if (!$account->retrieve('id', $user->kyte_account)) {
+                    throw new \Exception("Unable to find account associated with user");
+                }
+
+                // set user and account in class
+                $this->user = $user;
+                $this->account = $account;
+
+                if (USE_SESSION_MAP) {
+                    $response = $this->getObject($user);
+                } else {
+                    $response[] = $this->getObject($user);
+                }
             }
 
             // get api associated with account
             $account_api = new \Kyte\Core\ModelObject(APIKey);
-            if (!$account_api->retrieve('kyte_account', $account->id)) {
+            if (!$account_api->retrieve('kyte_account', $this->account->id)) {
                 throw new \Exception("[ERROR] Unable to find API information for account");
             }
 
