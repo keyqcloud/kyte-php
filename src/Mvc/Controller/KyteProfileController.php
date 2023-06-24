34be4a9c244172e178fcbd18fa1b92dc3cc81dd7
Kenneth P. Hough <kenneth@egtech.us>
2023-06-23 22:27:29 -0500

    Return user account object

--- src/Mvc/Controller/KyteProfileController.php
+++ src/Mvc/Controller/KyteProfileController.php
@@ -16,7 +16,16 @@ class KyteProfileController extends ModelController
 
     // public function hook_preprocess($method, &$r, &$o = null) {}
 
-    // public function hook_response_data($method, $o, &$r = null, &$d = null) {}
+    public function hook_response_data($method, $o, &$r = null, &$d = null) {
+        if ($method == 'get') {
+            $kyteAccount = new \Kyte\Core\ModelObject(KyteAccount);
+            if ($kyteAccount->retrieve('id', $r['kyte_account'])) {
+                $r['kyte_account'] = $this->getObject($kyteAccount);
+            } else {
+                $r['kyte_account'] = ['id' => $r['kyte_account']];
+            }
+        }
+    }
 
     // public function hook_process_get_response(&$r) {}
 }
