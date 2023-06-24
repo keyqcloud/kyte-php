6328a176c71ce9ed4ff079faa8628b59ead08faf
Kenneth P. Hough <kenneth@egtech.us>
2023-06-24 06:25:50 -0500

    Fix bug where stale data was returned

--- src/Core/ModelObject.php
+++ src/Core/ModelObject.php
@@ -229,7 +229,7 @@ class ModelObject
 			\Kyte\Core\DBI::update($this->kyte_model['name'], $id, $params, $types);
 
 			// populate with new data
-			populate($param);
+			$this->populate($param);
 
 			return true;
 		} catch (\Exception $e) {
