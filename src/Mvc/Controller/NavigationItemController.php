641aa98109e6c638cd928e05e485e2db4064f2d6
Kenneth P. Hough <kenneth@egtech.us>
2023-07-12 01:45:56 +0900

    No longer publish pages and update sitemap

--- src/Mvc/Controller/NavigationItemController.php
+++ src/Mvc/Controller/NavigationItemController.php
@@ -14,44 +14,12 @@ class NavigationItemController extends ModelController
 
     public function hook_response_data($method, $o, &$r = null, &$d = null) {
         switch ($method) {
-            case 'new':
-            case 'update':
-                $navitem = $r;
             case 'delete':
                 if ($method == 'delete') {
                     $navitem = $this->getObject($o);
                     $r = false;
                     $o->delete(null, null, $this->user->id);
                 }
-                // update pages with navigation
-                $credential = new \Kyte\Aws\Credentials($navitem['site']['region']);
-                $s3 = new \Kyte\Aws\S3($credential, $navitem['site']['s3BucketName']);
-
-                $pages = new \Kyte\Core\Model(Page);
-                $pages->retrieve("main_navigation", $navitem['navigation']['id']);
-                
-                $apiKey = new \Kyte\Core\ModelObject(APIKey);
-                if (!$apiKey->retrieve('kyte_account', $this->account->id)) {
-                    throw new \Exception("Unable to find key");
-                }
-
-                // iterate through each page
-                foreach($pages->objects as $page) {
-                    $p = $this->getObject($page);
-                    // compile html file
-                    $data = \Kyte\Mvc\Controller\PageController::createHtml($p);
-                    // write to file
-                    $s3->write($page->s3key, $data);
-                }
-
-                // create or update sitemap
-                $sitemap = \Kyte\Mvc\Controller\PageController::updateSitemap($navitem['site']['id'], $navitem['site']['aliasDomain'] ? $navitem['site']['aliasDomain'] : $navitem['site']['cfDomain']);
-                $s3->write('sitemap.xml', $sitemap);
-
-                // invalidate CF
-                $cf = new \Kyte\Aws\CloudFront($credential);
-                $invalidationPaths = ['/*'];
-                $cf->createInvalidation($navitem['site']['cfDistributionId'], $invalidationPaths);
                 break;
             
             default:
