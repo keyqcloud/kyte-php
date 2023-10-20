52e7323d56ca0f96bc71757680a56441184b15cd
Kenneth P. Hough <kenneth@egtech.us>
2023-10-20 15:59:35 -0500

    Add log output

--- src/Mvc/Controller/AppModelWrapper.php
+++ src/Mvc/Controller/AppModelWrapper.php
@@ -11,6 +11,7 @@ class AppModelWrapperController extends ModelController
     // public function hook_auth() {}
 
     public function hook_prequery($method, &$field, &$value, &$conditions, &$all, &$order) {
+        error_log("Get data info!");
         $appModel = new \Kyte\Core\ModelObject(DataModel);
         if (!$appModel->retrieve('application', $field, [['field' => 'name', 'value' => $value]])) {
             throw new \Exception('Unable to find model for specified application.');
@@ -21,12 +22,14 @@ class AppModelWrapperController extends ModelController
             throw new \Exception("CRITICAL ERROR: Unable to find application and perform context switch for app ID {$field}.");
         }
 
+        error_log("Loading new models");
         // load app specific models
         \Kyte\Core\Api::loadAppModelsAndControllers($field);
 
         // specify model of this wrapper controller
         $this->model = constant($value);
 
+        error_log("PREPARING DB SWITCH!");
         \Kyte\Core\Api::dbappconnect($this->api->app->db_name, $this->api->app->db_username, $this->api->app->db_password);
     }
 
