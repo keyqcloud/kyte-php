e1922b24c2d3bc4d248c1ca6b84df0a6433762fd
Kenneth P. Hough <kenneth@egtech.us>
2023-11-30 09:59:33 -0600

    Add wrapper for SQS

---
+++ src/Aws/Sqs.php
@@ -0,0 +1,61 @@
+<?php
+namespace Kyte\Aws;
+
+use Aws\Exception\AwsException;
+use Aws\Sqs\SqsClient;
+
+class Sqs extends Client
+{
+    private $queueUrl;
+    private $isFifo;
+
+    public function __construct($credentials, $queueUrl) {
+        $this->credentials = $credentials;
+        $this->queueUrl = $queueUrl;
+        $this->isFifo = strpos($this->queueUrl, '.fifo') !== false;
+
+        $this->client = new SqsClient([
+            'credentials' => $this->credentials->getCredentials(),
+            'version' => '2012-11-05',
+            'region'	=> $this->credentials->getRegion()
+        ]);
+    }
+
+    public function send($message, $messageGroupId = null) {
+        $param = [
+            'QueueUrl' => $this->queueUrl,
+            'MessageBody' => $message
+        ];
+        if ($this->isFifo) {
+            $param['MessageGroupId'] = $messageGroupId;
+        }
+        $result = $this->client->sendMessage($param);
+
+        if (!isset($result['MessageId'])) {
+            throw new \Exception("Unable to send message");
+        }
+
+        return $result['MessageId'];
+    }
+
+    public function receive() {
+        $param = [
+            'QueueUrl' => $this->queueUrl,
+            'MaxNumberOfMessages' => 1
+        ];
+        $result = $this->client->receiveMessage($param);
+
+        if (!isset($result['Messages'][0])) {
+            return null;
+        }
+
+        $message = $result['Messages'][0];
+        $param = [
+            'QueueUrl' => $this->queueUrl,
+            'ReceiptHandle' => $message['ReceiptHandle']
+        ];
+        $this->client->deleteMessage($param);
+
+        return $message;
+    }
+}
