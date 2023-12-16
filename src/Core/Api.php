f9e86410a6869f62e446ec24d5785b8cf8984de9
Kenneth P. Hough <kenneth@egtech.us>
2023-12-15 18:14:03 -0600

    Log to error prior to eval

--- src/Core/Api.php
+++ src/Core/Api.php
@@ -369,6 +369,8 @@ class Api
 		$controllers->retrieve("application", $app->id);
 		foreach($controllers->objects as $object) {
 			$code = bzdecompress($object->code);
+			error_log($object->name.' =======> ');
+			error_log($code);
 			eval($code);
 		}
 	}
