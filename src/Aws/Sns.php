b699459a53c0fc4af147b242720681713beb1158
Kenneth Hough <kenneth@keyqcloud.com>
2024-01-13 08:26:17 -0600

    Add wrapper for SNS

---
+++ src/Aws/Sns.php
@@ -0,0 +1,39 @@
+<?php
+namespace Kyte\Aws;
+
+use Aws\Exception\AwsException;
+use Aws\Sns\SqsClient;
+
+class Sns extends Client
+{
+    private $topicArn;
+
+    public function __construct($credentials, $topicArn) {
+        $this->credentials = $credentials;
+        $this->topicArn = $topicArn;
+
+        $this->client = new SqsClient([
+            'credentials' => $this->credentials->getCredentials(),
+            'version' => '2010-03-31',
+            'region'	=> $this->credentials->getRegion()
+        ]);
+    }
+
+    public function publish($message) {
+        if (!is_string($message)) {
+            $message = json_encode($message); // Convert to JSON string
+        }
+
+        $param = [
+            'TopicArn' => $this->topicArn,
+            'Message' => $message
+        ];
+        $result = $this->client->publish($param);
+
+        if (!isset($result['MessageId'])) {
+            throw new \Exception("Unable to send message");
+        }
+
+        return $result['MessageId'];
+    }
+}
