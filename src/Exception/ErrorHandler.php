0aaa45b338774b0958224a8ea0421a09900d16bc
Kenneth P. Hough <kenneth@egtech.us>
2024-04-01 09:29:09 -0500

    Add support for sending slack notification (#49)

--- src/Exception/ErrorHandler.php
+++ src/Exception/ErrorHandler.php
@@ -30,7 +30,7 @@ class ErrorHandler
             'user_id' => isset($this->apiContext->user->id) ? $this->apiContext->user->id : null,
             'app_id' => isset($this->apiContext->appId) ? $this->apiContext->appId : null,
             'api_key' => isset($this->apiContext->key) ? $this->apiContext->key : null,
-            //
+            // Additional details
             'signature' => isset($this->apiContext->signature) ? $this->apiContext->signature : null,
             'contentType' => isset($this->apiContext->contentType) ? $this->apiContext->contentType : null,
             'request' => isset($this->apiContext->request) ? $this->apiContext->request : null,
@@ -39,15 +39,45 @@ class ErrorHandler
             'value' => isset($this->apiContext->value) ? $this->apiContext->value : null,
             'data' => isset($this->apiContext->data) ? print_r($this->apiContext->data, true) : null,
             'response' => isset($this->apiContext->response) ? print_r($this->apiContext->response, true) : null,
-            //
+            // Exception details
             'message' => $exception->getMessage(),
             'line' => $exception->getLine(),
             'file' => $exception->getFile(),
         ];
+    
         if (!$error->create($log_detail)) {
-            error_log("FATA: Failed to create log entry for ".print_r($log_detail, true));
+            error_log("FATAL: Failed to create log entry for " . print_r($log_detail, true));
+        }
+    
+        $slackMessage = "Error encountered:\n" . print_r($log_detail, true);
+        // Platform level notification
+        if (defined('SLACK_ERROR_WEBHOOK')) {
+            $this->sendSlackNotification(SLACK_ERROR_WEBHOOK, $slackMessage);
+        }
+        // Application level notification
+        if ($this->apiContext->appId && $this->apiContext->app->slack_error_webhook) {
+            $this->sendSlackNotification($this->apiContext->app->slack_error_webhook, $slackMessage);
         }
     }
+    
+    private function sendSlackNotification($webhookUrl, $message) {
+        $payload = json_encode(['text' => $message]);
+        $ch = curl_init($webhookUrl);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($payload)
+        ]);
+    
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            error_log('Slack notification error: ' . curl_error($ch));
+        }
+        curl_close($ch);
+    }
+    
 
     public function handleError($errno, $errstr, $errfile, $errline) {
         // skip warnings, notices, deprecations for now
