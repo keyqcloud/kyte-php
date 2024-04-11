4918bd802654e939e8bf6da73d54bbe9a09f6f82
Kenneth P. Hough <kenneth@egtech.us>
2024-04-11 13:06:17 -0500

    Fix issue with code still using field (#53)

--- src/Mvc/Controller/AppModelWrapperController.php
+++ src/Mvc/Controller/AppModelWrapperController.php
@@ -14,14 +14,14 @@ class AppModelWrapperController extends ModelController
             throw new \Exception('Application model name must be provided.');
         }
 
-        $appModel = new \Kyte\Core\ModelObject(DataModel);
-        if (!$appModel->retrieve('application', $field, [['field' => 'name', 'value' => $_SERVER['HTTP_X_KYTE_APP_MODEL']]])) {
-            throw new \Exception('Unable to find model for specified application.');
-        }
-
         $this->api->app = new \Kyte\Core\ModelObject(Application);
         if (!$this->api->app->retrieve('id', $_SERVER['HTTP_X_KYTE_APP_ID'])) {
-            throw new \Exception("CRITICAL ERROR: Unable to find application and perform context switch for app ID {$field}.");
+            throw new \Exception("CRITICAL ERROR: Unable to find application and perform context switch for app ID {$_SERVER['HTTP_X_KYTE_APP_ID']}.");
+        }
+
+        $appModel = new \Kyte\Core\ModelObject(DataModel);
+        if (!$appModel->retrieve('application', $this->api->app->id, [['field' => 'name', 'value' => $_SERVER['HTTP_X_KYTE_APP_MODEL']]])) {
+            throw new \Exception('Unable to find model for specified application.');
         }
 
         // load app specific models
@@ -32,7 +32,7 @@ class AppModelWrapperController extends ModelController
 
         \Kyte\Core\Api::dbappconnect($this->api->app->db_name, $this->api->app->db_username, $this->api->app->db_password);
     }
-    
+
     // public function hook_auth() {}
 
     // public function hook_prequery($method, &$field, &$value, &$conditions, &$all, &$order) {}
