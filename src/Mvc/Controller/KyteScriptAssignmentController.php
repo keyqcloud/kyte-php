c4a71913db5e6cddcc7703da73bc38ea7a4595c0
Kenneth Hough <kenneth@keyqcloud.com>
2025-08-11 11:56:05 +0900

    Prevent deletion of globally included scripts from assignment (#11)

--- src/Mvc/Controller/KyteScriptAssignmentController.php
+++ src/Mvc/Controller/KyteScriptAssignmentController.php
@@ -1,17 +1,22 @@
 <?php
-
 namespace Kyte\Mvc\Controller;
 
 class KyteScriptAssignmentController extends ModelController
 {
     public function hook_response_data($method, $o, &$r = null, &$d = null) {
-        if (isset($r['script'])) {
-            $r['script']['content'] = '';
-            $r['script']['content_js_obfuscated'] = '';
-        }
-        if (isset($r['page'], $r['page']['header'], $r['page']['footer'])) {
-            $r['page']['header'] = null;
-            $r['page']['footer'] = null;
+        if ($method == 'delete') {
+            if ($o->include_all == 1) {
+                throw new \Exception("Cannot delete assignment for globally included script. This script is set to include on all pages. To remove it, either disable global inclusion in Site → Custom Scripts or delete the script entirely.");
+            }
+        } else {
+            if (isset($r['script'])) {
+                $r['script']['content'] = '';
+                $r['script']['content_js_obfuscated'] = '';
+            }
+            if (isset($r['page'], $r['page']['header'], $r['page']['footer'])) {
+                $r['page']['header'] = null;
+                $r['page']['footer'] = null;
+            }
         }
     }
 }
