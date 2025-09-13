c1e8c0c7969325641d0d7db339e7cf2fb5add909
Kenneth Hough <kenneth@keyqcloud.com>
2025-09-13 17:04:36 +0900

    Add support for republishing and updating kyte_connect

--- src/Mvc/Controller/ApplicationController.php
+++ src/Mvc/Controller/ApplicationController.php
@@ -74,6 +74,69 @@ class ApplicationController extends ModelController
 
     public function hook_response_data($method, $o, &$r = null, &$d = null) {
         switch ($method) {
+            case 'update':
+                if (isset($d['republish_kyte_connect']) && $d['republish_kyte_connect'] == 1) {
+                    $sites = new \Kyte\Core\ModelObject(KyteSite);
+                    $sites->retrieve('application', $o->id);
+                    foreach($sites->objects as $site) {
+                        $credential = new \Kyte\Aws\Credentials($site->region, $app->aws_public_key, $app->aws_private_key);
+                        $s3 = new \Kyte\Aws\S3($credential, $site->s3BucketName);
+
+                        $pages = new \Kyte\Core\Model(KytePage);
+                        $pages->retrieve("state", 1, false, [['field' => 'site', 'value' => $site->id]]);
+
+                        foreach($pages->objects as $page) {
+                            $params = $this->getObject($page);
+                            $pd = new \Kyte\Core\ModelObject(KytePageData);
+                            if (!$pd->retrieve('page', $page->id)) {
+                                throw new \Exception("CRITICAL ERROR: Unable to find page data.");
+                            }
+
+                            $params['html'] = bzdecompress($pd->html);
+                            $params['stylesheet'] = bzdecompress($pd->stylesheet);
+                            $params['javascript'] = bzdecompress($pd->javascript);
+                            $params['javascript_obfuscated'] = bzdecompress($pd->javascript_obfuscated);
+                            // footers and headers
+                            if ($params['footer'] && isset($params['footer']['html'], $params['footer']['stylesheet'], $params['footer']['javascript'], $params['footer']['javascript_obfuscated'], $params['footer']['block_layout'])) {
+                                $params['footer']['html'] = bzdecompress($params['footer']['html']);
+                                $params['footer']['stylesheet'] = bzdecompress($params['footer']['stylesheet']);
+                                $params['footer']['javascript'] = bzdecompress($params['footer']['javascript']);
+                                $params['footer']['javascript_obfuscated'] = bzdecompress($params['footer']['javascript_obfuscated']);
+                                $params['footer']['block_layout'] = bzdecompress($params['footer']['block_layout']);
+                            }
+                            if ($params['header'] && isset($params['header']['html'], $params['header']['stylesheet'], $params['header']['javascript'], $params['header']['javascript_obfuscated'], $params['header']['block_layout'])) {
+                                $params['header']['html'] = bzdecompress($params['header']['html']);
+                                $params['header']['stylesheet'] = bzdecompress($params['header']['stylesheet']);
+                                $params['header']['javascript'] = bzdecompress($params['header']['javascript']);
+                                $params['header']['javascript_obfuscated'] = bzdecompress($params['header']['javascript_obfuscated']);
+                                $params['header']['block_layout'] = bzdecompress($params['header']['block_layout']);
+                            }
+                            // compile html file
+                            $data = \Kyte\Mvc\Controller\KytePageController::createHtml($params);
+                            // write to file
+                            $s3->write($page->s3key, $data);
+                        }
+                    }
+
+                    // invalidate CF
+                    $invalidationPaths = ['/*'];
+                    if (KYTE_USE_SNS) {
+                        $credential = new \Kyte\Aws\Credentials(SNS_REGION);
+                        $sns = new \Kyte\Aws\Sns($credential, SNS_QUEUE_SITE_MANAGEMENT);
+                        $sns->publish([
+                            'action' => 'cf_invalidate',
+                            'site_id' => $site->id,
+                            'cf_id' => $rsite->cfDistributionId,
+                            'cf_invalidation_paths' => $invalidationPaths,
+                            'caller_id' => time(),
+                        ]);
+                    } else {
+                        // invalidate CF
+                        $cf = new \Kyte\Aws\CloudFront($credential);
+                        $cf->createInvalidation($site->cfDistributionId, $invalidationPaths);
+                    }
+                }
+                break;
             case 'delete':
                 // // get AWS credentials
                 // $credentials = new \Kyte\Aws\Credentials('us-east-1');
