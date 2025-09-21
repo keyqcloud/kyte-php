657f7b8421bea0350ef3a06f0a19d456f3c30aa2
Kenneth Hough <kenneth@keyqcloud.com>
2025-09-21 18:06:25 +0900

    Add controllers for global assingments

---
+++ src/Mvc/Controller/KyteLibraryGlobalAssignmentController.php
@@ -0,0 +1,17 @@
+<?php
+namespace Kyte\Mvc\Controller;
+
+class KyteLibraryGlobalAssignmentController extends ModelController
+{
+    public function hook_init() {
+        // for global assignments, disallow direct create/update/delete
+        $this->allowableActions = ['get'];
+    }
+
+    public function hook_prequery($method, &$field, &$value, &$conditions, &$all, &$order) {
+        if (!is_array($conditions)) {
+            $conditions = [];
+        }
+        $conditions[] = ['field' => 'include_all', 'value' => 1];
+    }
+}
