5f56157fbb4f212e53bfe1128c6463444f4f3ff5
Kenneth Hough <kenneth@keyqcloud.com>
2025-09-21 17:30:24 +0900

    Fix fk table association for checking if include_all is set

--- src/Mvc/Controller/KyteLibraryAssignmentController.php
+++ src/Mvc/Controller/KyteLibraryAssignmentController.php
@@ -15,7 +15,8 @@ class KyteLibraryAssignmentController extends ModelController
     
     public function hook_response_data($method, $o, &$r = null, &$d = null) {
         if ($method == 'delete') {
-            if ($o->include_all == 1) {
+            $ret = $this->getObject($o);
+            if (isset($ret['library']['include_all']) && $ret['library']['include_all'] == 1) {
                 throw new \Exception("Cannot delete assignment for globally included libraries. This library is set to include on all pages. To remove it, either disable global inclusion in Site → Libraries or delete the library entirely.");
             }
         } else {
--- src/Mvc/Controller/KyteScriptAssignmentController.php
+++ src/Mvc/Controller/KyteScriptAssignmentController.php
@@ -15,7 +15,8 @@ class KyteScriptAssignmentController extends ModelController
 
     public function hook_response_data($method, $o, &$r = null, &$d = null) {
         if ($method == 'delete') {
-            if ($o->include_all == 1) {
+            $ret = $this->getObject($o);
+            if (isset($ret['library']['include_all']) && $ret['library']['include_all'] == 1) {
                 throw new \Exception("Cannot delete assignment for globally included script. This script is set to include on all pages. To remove it, either disable global inclusion in Site → Custom Scripts or delete the script entirely.");
             }
         } else {
