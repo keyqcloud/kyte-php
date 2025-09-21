f643f17ba7ed57a3bae038f811fa3a9f0aa9c3fb
Kenneth Hough <kenneth@keyqcloud.com>
2025-09-21 17:27:26 +0900

    Update to move validation check in preprocess hook

--- src/Mvc/Controller/KyteLibraryAssignmentController.php
+++ src/Mvc/Controller/KyteLibraryAssignmentController.php
@@ -3,19 +3,22 @@ namespace Kyte\Mvc\Controller;
 
 class KyteLibraryAssignmentController extends ModelController
 {
+    public function hook_preprocess($method, &$r, &$o = null) {
+        if ($method == 'new') {
+            $assignments = new \Kyte\Core\Model($this->model);
+            $assignments->retrieve('library', $d['library'], false, [['field' => 'page', 'value' => $d['page']], ['field' => 'site', 'value' => $d['site']], ['field' => 'kyte_account', 'value' => $this->account->id]]);
+            if ($assignments->count > 0) {
+                throw new \Exception("This library is already assigned to the specified page or site.");
+            }
+        }
+    }
+    
     public function hook_response_data($method, $o, &$r = null, &$d = null) {
         if ($method == 'delete') {
             if ($o->include_all == 1) {
                 throw new \Exception("Cannot delete assignment for globally included libraries. This library is set to include on all pages. To remove it, either disable global inclusion in Site → Libraries or delete the library entirely.");
             }
         } else {
-            if ($method == 'new') {
-                $assignments = new \Kyte\Core\Model($this->model);
-                $assignments->retrieve('library', $d['library'], false, [['field' => 'page', 'value' => $d['page']], ['field' => 'site', 'value' => $d['site']], ['field' => 'kyte_account', 'value' => $this->account->id]]);
-                if ($assignments->count > 0) {
-                    throw new \Exception("This library is already assigned to the specified page or site.");
-                }
-            }
             if (isset($r['page'], $r['page']['header'], $r['page']['footer'])) {
                 $r['page']['header'] = null;
                 $r['page']['footer'] = null;
--- src/Mvc/Controller/KyteScriptAssignmentController.php
+++ src/Mvc/Controller/KyteScriptAssignmentController.php
@@ -3,19 +3,22 @@ namespace Kyte\Mvc\Controller;
 
 class KyteScriptAssignmentController extends ModelController
 {
+    public function hook_preprocess($method, &$r, &$o = null) {
+        if ($method == 'new') {
+            $assignments = new \Kyte\Core\Model($this->model);
+            $assignments->retrieve('script', $d['library'], false, [['field' => 'page', 'value' => $d['page']], ['field' => 'site', 'value' => $d['site']], ['field' => 'kyte_account', 'value' => $this->account->id]]);
+            if ($assignments->count > 0) {
+                throw new \Exception("This custom script is already assigned to the specified page or site.");
+            }
+        }
+    }
+
     public function hook_response_data($method, $o, &$r = null, &$d = null) {
         if ($method == 'delete') {
             if ($o->include_all == 1) {
                 throw new \Exception("Cannot delete assignment for globally included script. This script is set to include on all pages. To remove it, either disable global inclusion in Site → Custom Scripts or delete the script entirely.");
             }
         } else {
-            if ($method == 'new') {
-                $assignments = new \Kyte\Core\Model($this->model);
-                $assignments->retrieve('script', $d['library'], false, [['field' => 'page', 'value' => $d['page']], ['field' => 'site', 'value' => $d['site']], ['field' => 'kyte_account', 'value' => $this->account->id]]);
-                if ($assignments->count > 0) {
-                    throw new \Exception("This custom script is already assigned to the specified page or site.");
-                }
-            }
             if (isset($r['script'])) {
                 $r['script']['content'] = '';
                 $r['script']['content_js_obfuscated'] = '';
