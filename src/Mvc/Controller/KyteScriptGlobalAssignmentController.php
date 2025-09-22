986538ce43f2cd706c087e38701d061e3dea60c7
Kenneth P. Hough <kenneth@egtech.us>
2025-09-22 12:11:28 +0900

    Omit compressed data from response

--- src/Mvc/Controller/KyteScriptGlobalAssignmentController.php
+++ src/Mvc/Controller/KyteScriptGlobalAssignmentController.php
@@ -17,6 +17,10 @@ class KyteScriptGlobalAssignmentController extends ModelController
     }
 
     public function hook_response_data($method, $o, &$r = null, &$d = null) {
+        if (isset($r['script'])) {
+                $r['script']['content'] = '';
+                $r['script']['content_js_obfuscated'] = '';
+            }
         if (isset($r['page'], $r['page']['header'], $r['page']['footer'])) {
             $r['page']['header'] = null;
             $r['page']['footer'] = null;
