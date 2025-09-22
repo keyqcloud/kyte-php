d950e7773280d0993b1832f9ab480f8ee7903b57
Kenneth P. Hough <kenneth@egtech.us>
2025-09-22 10:02:06 +0900

    Refactor and optimize function and controller codes

--- src/Mvc/Controller/ControllerController.php
+++ src/Mvc/Controller/ControllerController.php
@@ -4,46 +4,31 @@ namespace Kyte\Mvc\Controller;
 
 class ControllerController extends ModelController
 {
-    // public function hook_init() {
-    //     $this->checkExisting = 'name';
-    // }
+    // Namespace patterns for validation
+    private const KYTE_CORE_NAMESPACE = '\\Kyte\\Mvc\\Controller\\';
+    private const CONTROLLER_SUFFIX = 'Controller';
 
-    // public function hook_auth() {}
+    // Error messages
+    private const ERROR_MESSAGES = [
+        'name_exists_scope' => 'Controller name already exists in application scope.',
+        'name_exists_new_scope' => 'New controller name already exists in application scope.',
+        'name_kyte_core' => 'Controller name already in use by Kyte core API.',
+        'name_custom' => 'Custom controller name already in use.',
+        'model_not_found' => 'Unable to find specified data model.'
+    ];
 
-    // public function hook_prequery($method, &$field, &$value, &$conditions, &$all, &$order) {}
+    // Template for generated code
+    private const CODE_TEMPLATE = "class %sController extends \\Kyte\\Mvc\\Controller\\ModelController\r\n{\r\n%s}\r\n";
+    private const SHIPYARD_INIT_TEMPLATE = "\tpublic function shipyard_init() {\r\n\t\t\$this->model = %s;\r\n\t}\r\n";
 
     public function hook_preprocess($method, &$r, &$o = null) {
         switch ($method) {
             case 'new':
-                $existingController = new \Kyte\Core\ModelObject(constant('Controller'));
-                if ($existingController->retrieve('name', $r['name'], [['field' => 'application', 'value' => $r['application']]])) {
-                    throw new \Exception("Controller name already exists in application scope.");
-                }
-                // check if new name is unique
-                if (class_exists('\\Kyte\Mvc\\Controller\\'.$r['name'].'Controller')) {
-                    throw new \Exception("Controller name already in use by Kyte core API.");
-                }
-                if (class_exists($r['name'].'Controller')){
-                    throw new \Exception("Custom controller name already in use.");
-                }
+                $this->validateControllerName($r['name'], $r['application']);
                 break;
-
             case 'update':
-                if ($o->name != $r['name']) {
-                    $existingController = new \Kyte\Core\ModelObject(constant('Controller'));
-                    if ($existingController->retrieve('name', $r['name'], [['field' => 'application', 'value' => $r['application']]])) {
-                        throw new \Exception("New controller name already exists in application scope.");
-                    }
-                    // check if new name is unique
-                    if (class_exists('\\Kyte\Mvc\\Controller\\'.$r['name'].'Controller')) {
-                        throw new \Exception("Controller name already in use by Kyte core API.");
-                    }
-                    if (class_exists($r['name'].'Controller')){
-                        throw new \Exception("Custom controller name already in use.");
-                    }
-                }
-                break;                
-
+                $this->validateControllerUpdate($o, $r);
+                break;
             default:
                 break;
         }
@@ -53,55 +38,153 @@ class ControllerController extends ModelController
         switch ($method) {
             case 'new':
             case 'update':
-                // update code base and save to file
-                self::generateCodeBase($o);
+                $this->regenerateControllerCode($o);
                 break;
             case 'delete':
-                // delete corresponding function association
-                $fs = new \Kyte\Core\Model(constant("Function"));
-                $fs->retrieve("controller", $o->id);
-                foreach($fs->objects as $f) {
-                    $f->delete();
-                }
+                $this->cleanupControllerFunctions($o);
                 break;
-            
             default:
                 break;
         }
     }
 
-    // public function hook_process_get_response(&$r) {}
+    /**
+     * Validate controller name for new controllers
+     */
+    private function validateControllerName(string $name, int $applicationId): void
+    {
+        $this->checkNameExistsInScope($name, $applicationId, 'name_exists_scope');
+        $this->checkNameConflicts($name);
+    }
 
-    public static function generateCodeBase($controller) {
-        $functions = [];
-
-        // check if model is specified, if so generate shipyard_init()
-        if (!empty($controller->dataModel)) {
-            $model = new \Kyte\Core\ModelObject(DataModel);
-            if (!$model->retrieve("id", $controller->dataModel)) {
-                throw new \Exception("Unable to find specified data model.");
-            }
-            $functions[] = "\tpublic function shipyard_init() {\r\n\t\t\$this->model = {$model->name};\r\n\t}\r\n";
+    /**
+     * Validate controller name for updates
+     */
+    private function validateControllerUpdate($originalController, array $updateData): void
+    {
+        // Only validate if the name is actually changing
+        if ($originalController->name === $updateData['name']) {
+            return;
         }
 
-        $fs = new \Kyte\Core\Model(constant("Function"));
-        $fs->retrieve("controller", $controller->id);
-        foreach($fs->objects as $f) {
-            $functions[] = bzdecompress($f->code);
+        $this->checkNameExistsInScope(
+            $updateData['name'], 
+            $updateData['application'], 
+            'name_exists_new_scope'
+        );
+        $this->checkNameConflicts($updateData['name']);
+    }
+
+    /**
+     * Check if controller name already exists in application scope
+     */
+    private function checkNameExistsInScope(string $name, int $applicationId, string $errorKey): void
+    {
+        $existingController = new \Kyte\Core\ModelObject(constant('Controller'));
+        $conditions = [['field' => 'application', 'value' => $applicationId]];
+        
+        if ($existingController->retrieve('name', $name, $conditions)) {
+            throw new \Exception(self::ERROR_MESSAGES[$errorKey]);
+        }
+    }
+
+    /**
+     * Check for name conflicts with existing classes
+     */
+    private function checkNameConflicts(string $name): void
+    {
+        $kyteCoreName = self::KYTE_CORE_NAMESPACE . $name . self::CONTROLLER_SUFFIX;
+        $customName = $name . self::CONTROLLER_SUFFIX;
+
+        if (class_exists($kyteCoreName)) {
+            throw new \Exception(self::ERROR_MESSAGES['name_kyte_core']);
         }
 
-        $code = "class {$controller->name}Controller extends \Kyte\Mvc\Controller\ModelController\r\n{\r\n";
-
-        if (is_array($functions)) {
-            foreach($functions as $function) {
-                $code .= "\r\n$function\r\n";
-            }
+        if (class_exists($customName)) {
+            throw new \Exception(self::ERROR_MESSAGES['name_custom']);
         }
+    }
 
-        $code .= "}\r\n";
+    /**
+     * Regenerate controller code and save to file
+     */
+    private function regenerateControllerCode($controller): void
+    {
+        self::generateCodeBase($controller);
+    }
 
+    /**
+     * Clean up associated functions when controller is deleted
+     */
+    private function cleanupControllerFunctions($controller): void
+    {
+        $functions = new \Kyte\Core\Model(constant("Function"));
+        $functions->retrieve("controller", $controller->id);
+        
+        foreach ($functions->objects as $function) {
+            $function->delete();
+        }
+    }
+
+    /**
+     * Generate the complete code base for a controller
+     */
+    public static function generateCodeBase($controller): void
+    {
+        $functions = self::collectControllerFunctions($controller);
+        $code = self::buildControllerCode($controller->name, $functions);
+        
         $controller->save([
             'code' => bzcompress($code, 9),
         ]);
+    }
+
+    /**
+     * Collect all functions for the controller
+     */
+    private static function collectControllerFunctions($controller): array
+    {
+        $functions = [];
+
+        // Add shipyard_init if model is specified
+        if (!empty($controller->dataModel)) {
+            $functions[] = self::generateShipyardInit($controller->dataModel);
+        }
+
+        // Add custom functions
+        $customFunctions = new \Kyte\Core\Model(constant("Function"));
+        $customFunctions->retrieve("controller", $controller->id);
+        
+        foreach ($customFunctions->objects as $function) {
+            $functions[] = bzdecompress($function->code);
+        }
+
+        return $functions;
+    }
+
+    /**
+     * Generate shipyard_init function code
+     */
+    private static function generateShipyardInit(int $dataModelId): string
+    {
+        $model = new \Kyte\Core\ModelObject(DataModel);
+        if (!$model->retrieve("id", $dataModelId)) {
+            throw new \Exception(self::ERROR_MESSAGES['model_not_found']);
+        }
+
+        return sprintf(self::SHIPYARD_INIT_TEMPLATE, $model->name);
+    }
+
+    /**
+     * Build the complete controller class code
+     */
+    private static function buildControllerCode(string $controllerName, array $functions): string
+    {
+        $functionsCode = '';
+        if (!empty($functions)) {
+            $functionsCode = "\r\n" . implode("\r\n", $functions) . "\r\n";
+        }
+
+        return sprintf(self::CODE_TEMPLATE, $controllerName, $functionsCode);
     }
 }
--- src/Mvc/Controller/FunctionController.php
+++ src/Mvc/Controller/FunctionController.php
@@ -4,103 +4,63 @@ namespace Kyte\Mvc\Controller;
 
 class FunctionController extends ModelController
 {
-    // public function hook_init() {}
+    // Configuration for function types and their templates
+    private const FUNCTION_TYPES = [
+        'hook_init' => [
+            'template' => "public function hook_init() {\r\n\t\r\n}\r\n",
+            'is_hook' => true
+        ],
+        'hook_auth' => [
+            'template' => "public function hook_auth() {\r\n\t\r\n}\r\n",
+            'is_hook' => true
+        ],
+        'hook_prequery' => [
+            'template' => "public function hook_prequery(\$method, &\$field, &\$value, &\$conditions, &\$all, &\$order) {\r\n{switch_statement}}\r\n",
+            'is_hook' => true
+        ],
+        'hook_preprocess' => [
+            'template' => "public function hook_preprocess(\$method, &\$r, &\$o = null) {\r\n{switch_statement}}\r\n",
+            'is_hook' => true
+        ],
+        'hook_response_data' => [
+            'template' => "public function hook_response_data(\$method, \$o, &\$r = null, &\$d = null) {\r\n{switch_statement}}\r\n",
+            'is_hook' => true
+        ],
+        'hook_process_get_response' => [
+            'template' => "public function hook_process_get_response(&\$r) {\r\n\r\n}\r\n",
+            'is_hook' => true
+        ],
+        'new' => [
+            'template' => "public function new(\$data) {\r\n\r\n}\r\n",
+            'is_hook' => false
+        ],
+        'update' => [
+            'template' => "public function update(\$field, \$value, \$data) {\r\n\r\n}\r\n",
+            'is_hook' => false
+        ],
+        'get' => [
+            'template' => "public function get(\$field, \$value) {\r\n\r\n}\r\n",
+            'is_hook' => false
+        ],
+        'delete' => [
+            'template' => "public function delete(\$field, \$value) {\r\n\r\n}\r\n",
+            'is_hook' => false
+        ]
+    ];
 
-    // public function hook_auth() {}
+    private const SWITCH_STATEMENT = "\tswitch (\$method) {\r\n\t\tcase 'new':\r\n\t\t\tbreak;\r\n\r\n\t\tcase 'update':\r\n\t\t\tbreak;\r\n\r\n\t\tcase 'get':\r\n\t\t\tbreak;\r\n\r\n\t\tcase 'delete':\r\n\t\t\tbreak;\r\n\r\n\t\tdefault:\r\n\t\t\tbreak;\r\t}\r\n";
 
-    // public function hook_prequery($method, &$field, &$value, &$conditions, &$all, &$order) {}
+    // Methods that require controller code base regeneration
+    private const REGENERATION_METHODS = ['update', 'delete'];
 
     public function hook_preprocess($method, &$r, &$o = null) {
         switch ($method) {
             case 'new':
-                $switch_stmt = "\tswitch (\$method) {\r\n\t\tcase 'new':\r\n\t\t\tbreak;\r\n\r\n\t\tcase 'update':\r\n\t\t\tbreak;\r\n\r\n\t\tcase 'get':\r\n\t\t\tbreak;\r\n\r\n\t\tcase 'delete':\r\n\t\t\tbreak;\r\n\r\n\t\tdefault:\r\n\t\t\tbreak;\r\t}\r\n";
-                $code = '';
-                if ($r['type'] == 'hook_init') {
-                    $f = new \Kyte\Core\Model(constant("Function"));
-                    $f->retrieve('controller', $r['controller'], false, [['field' => 'type', 'value' => $r['type']]]);
-                    if ($f->count() > 0) {
-                        throw new \Exception('Hook of type '.$r['type'].' already exists for this controller.');
-                    }
-                    $code = "public function hook_init() {\r\n\t\r\n}\r\n";
-                }
-                if ($r['type'] == 'hook_auth') {
-                    $f = new \Kyte\Core\Model(constant("Function"));
-                    $f->retrieve('controller', $r['controller'], false, [['field' => 'type', 'value' => $r['type']]]);
-                    if ($f->count() > 0) {
-                        throw new \Exception('Hook of type '.$r['type'].' already exists for this controller.');
-                    }
-                    $code = "public function hook_auth() {\r\n\t\r\n}\r\n";
-                }
-                if ($r['type'] == 'hook_prequery') {
-                    $f = new \Kyte\Core\Model(constant("Function"));
-                    $f->retrieve('controller', $r['controller'], false, [['field' => 'type', 'value' => $r['type']]]);
-                    if ($f->count() > 0) {
-                        throw new \Exception('Hook of type '.$r['type'].' already exists for this controller.');
-                    }
-                    $code = "public function hook_prequery(\$method, &\$field, &\$value, &\$conditions, &\$all, &\$order) {\r\n$switch_stmt}\r\n";
-                }
-                if ($r['type'] == 'hook_preprocess') {
-                    $f = new \Kyte\Core\Model(constant("Function"));
-                    $f->retrieve('controller', $r['controller'], false, [['field' => 'type', 'value' => $r['type']]]);
-                    if ($f->count() > 0) {
-                        throw new \Exception('Hook of type '.$r['type'].' already exists for this controller.');
-                    }
-                    $code = "public function hook_preprocess(\$method, &\$r, &\$o = null) {\r\n$switch_stmt}\r\n";
-                }
-                if ($r['type'] == 'hook_response_data') {
-                    $f = new \Kyte\Core\Model(constant("Function"));
-                    $f->retrieve('controller', $r['controller'], false, [['field' => 'type', 'value' => $r['type']]]);
-                    if ($f->count() > 0) {
-                        throw new \Exception('Hook of type '.$r['type'].' already exists for this controller.');
-                    }
-                    $code = "public function hook_response_data(\$method, \$o, &\$r = null, &\$d = null) {\r\n$switch_stmt}\r\n";
-                }
-                if ($r['type'] == 'hook_process_get_response') {
-                    $f = new \Kyte\Core\Model(constant("Function"));
-                    $f->retrieve('controller', $r['controller'], false, [['field' => 'type', 'value' => $r['type']]]);
-                    if ($f->count() > 0) {
-                        throw new \Exception('Hook of type '.$r['type'].' already exists for this controller.');
-                    }
-                    $code = "public function hook_process_get_response(&\$r) {\r\n\r\n}\r\n";
-                }
-                if ($r['type'] == 'new') {
-                    $f = new \Kyte\Core\Model(constant("Function"));
-                    $f->retrieve('controller', $r['controller'], false, [['field' => 'type', 'value' => $r['type']]]);
-                    if ($f->count() > 0) {
-                        throw new \Exception('Override of type '.$r['type'].' already exists for this controller.');
-                    }
-                    $code = "public function new(\$data) {\r\n\r\n}\r\n";
-                }
-                if ($r['type'] == 'update') {
-                    $f = new \Kyte\Core\Model(constant("Function"));
-                    $f->retrieve('controller', $r['controller'], false, [['field' => 'type', 'value' => $r['type']]]);
-                    if ($f->count() > 0) {
-                        throw new \Exception('Override of type '.$r['type'].' already exists for this controller.');
-                    }
-                    $code = "public function update(\$field, \$value, \$data) {\r\n\r\n}\r\n";
-                }
-                if ($r['type'] == 'get') {
-                    $f = new \Kyte\Core\Model(constant("Function"));
-                    $f->retrieve('controller', $r['controller'], false, [['field' => 'type', 'value' => $r['type']]]);
-                    if ($f->count() > 0) {
-                        throw new \Exception('Override of type '.$r['type'].' already exists for this controller.');
-                    }
-                    $code = "public function get(\$field, \$value) {\r\n\r\n}\r\n";
-                }
-                if ($r['type'] == 'delete') {
-                    $f = new \Kyte\Core\Model(constant("Function"));
-                    $f->retrieve('controller', $r['controller'], false, [['field' => 'type', 'value' => $r['type']]]);
-                    if ($f->count() > 0) {
-                        throw new \Exception('Override of type '.$r['type'].' already exists for this controller.');
-                    }
-                    $code = "public function delete(\$field, \$value) {\r\n\r\n}\r\n";
-                }
-                $r['code'] = bzcompress($code, 9);
+                $this->processNewFunction($r);
                 break;
             case 'update':
-                $r['code'] = bzcompress($r['code'], 9);
+                $this->processUpdateFunction($r);
                 break;
-
             default:
                 break;
         }
@@ -110,32 +70,97 @@ class FunctionController extends ModelController
         switch ($method) {
             case 'new':
             case 'get':
-                $r['code'] = bzdecompress($r['code']);
+                $this->decompressCode($r);
                 break;
             case 'update':
-                $ctrl = new \Kyte\Core\ModelObject(constant("Controller"));
-                if (!$ctrl->retrieve("id", $o->controller)) {
-                    throw new \Exception("Unable to find specified controller.");
+            case 'delete':
+                $this->handleCodeRegeneration($o, $method);
+                if ($method === 'update') {
+                    $this->decompressCode($r);
                 }
-
-                // update code base and save to file
-                ControllerController::generateCodeBase($ctrl);
-
-                $r['code'] = bzdecompress($r['code']);
                 break;
-            case 'delete':                
-                $ctrl = new \Kyte\Core\ModelObject(constant("Controller"));
-                if (!$ctrl->retrieve("id", $o->controller)) {
-                    throw new \Exception("Unable to find specified controller.");
-                }
-
-                // update code base and save to file
-                ControllerController::generateCodeBase($ctrl);
-                break;            
             default:
                 break;
         }
     }
 
-    // public function hook_process_get_response(&$r) {}
+    /**
+     * Process new function creation
+     */
+    private function processNewFunction(array &$r): void
+    {
+        $type = $r['type'] ?? '';
+        
+        if (!isset(self::FUNCTION_TYPES[$type])) {
+            throw new \InvalidArgumentException("Invalid function type: {$type}");
+        }
+
+        $this->validateFunctionUniqueness($r['controller'], $type);
+        
+        $config = self::FUNCTION_TYPES[$type];
+        $code = $this->generateCodeFromTemplate($config['template']);
+        
+        $r['code'] = bzcompress($code, 9);
+    }
+
+    /**
+     * Process function update
+     */
+    private function processUpdateFunction(array &$r): void
+    {
+        $r['code'] = bzcompress($r['code'], 9);
+    }
+
+    /**
+     * Validate that function doesn't already exist for the controller
+     */
+    private function validateFunctionUniqueness(int $controllerId, string $type): void
+    {
+        $functionModel = new \Kyte\Core\Model(constant("Function"));
+        $functionModel->retrieve('controller', $controllerId, false, [
+            ['field' => 'type', 'value' => $type]
+        ]);
+
+        if ($functionModel->count() > 0) {
+            $config = self::FUNCTION_TYPES[$type];
+            $errorType = $config['is_hook'] ? 'Hook' : 'Override';
+            throw new \Exception("{$errorType} of type {$type} already exists for this controller.");
+        }
+    }
+
+    /**
+     * Generate code from template
+     */
+    private function generateCodeFromTemplate(string $template): string
+    {
+        return str_replace('{switch_statement}', self::SWITCH_STATEMENT, $template);
+    }
+
+    /**
+     * Decompress code data
+     */
+    private function decompressCode(array &$r): void
+    {
+        if (isset($r['code'])) {
+            $r['code'] = bzdecompress($r['code']);
+        }
+    }
+
+    /**
+     * Handle controller code base regeneration
+     */
+    private function handleCodeRegeneration($o, string $method): void
+    {
+        if (!in_array($method, self::REGENERATION_METHODS)) {
+            return;
+        }
+
+        $ctrl = new \Kyte\Core\ModelObject(constant("Controller"));
+        if (!$ctrl->retrieve("id", $o->controller)) {
+            throw new \Exception("Unable to find specified controller.");
+        }
+
+        // Update code base and save to file
+        ControllerController::generateCodeBase($ctrl);
+    }
 }
